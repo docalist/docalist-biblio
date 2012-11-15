acfcdb0dbacc267ef25fbce5f3ff8c9d5d2d3283
Daniel Ménard <daniel-menard@users.noreply.github.com>
2012-11-15 22:13:15 +0000

    Ajout de index.php pour indiquer à wordpress qu'il s'agit d'un plugin.

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Docalist-bilio
+Plugin URI: http://docalist.org
+Description: Docalist, gestion des notices bibliographiques.
+Version: 1.0
+Author: Docalist
+Author URI: http://docalist.org
+*/
