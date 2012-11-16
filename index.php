d37c10085c0e6b58d31a18d812a7c77b5aa4ddf9
Daniel Ménard <daniel-menard@users.noreply.github.com>
2012-11-16 18:42:31 +0000

    Suppression du fichier index.php créé initialement (remplacé par main.php).

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin Name: Docalist-biblio
-Plugin URI: http://docalist.org
-Description: Docalist : gestion de notices bibliographiques.
-Version: 1.0
-Author: Docalist
-Author URI: http://docalist.org
-*/
