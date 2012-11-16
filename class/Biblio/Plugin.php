d40795c37c004239d3f2f676a23e85d3bd40405e
Daniel Ménard <daniel-menard@users.noreply.github.com>
2012-11-16 18:33:41 +0000

    Classe de base du plugin docalist-biblio.

---
+++ class/Biblio/Plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of a "Docalist Biblio" plugin.
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Biblio
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+
+namespace Docalist\Biblio;
+use Docalist\Core\AbstractPlugin;
+
+/**
+ * Plugin de gestion de notices bibliographiques.
+ */
+class Plugin extends AbstractPlugin {
+}
