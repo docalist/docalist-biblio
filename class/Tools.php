56255e88b60ff94b60b1bc78064937e2a581852b
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-05-22 11:01:31 +0000

    Import : change le path du fichier pour charger la V4 de la base prisme.

--- class/Tools.php
+++ class/Tools.php
@@ -92,7 +92,8 @@ class Tools extends AbstractAdminPage {
 
         if (true) {
 //          $path = 'd:/prisme/BASE PRISME NOUVEAU FORMAT.TXT';
-            $path = $uploadDir . '/prisme/BaseV3/BDD PRISME separateur habituel.txt';
+//            $path = $uploadDir . '/prisme/BaseV3/BDD PRISME separateur habituel.txt';
+            $path = $uploadDir . '/prisme/BaseV4/BDDPrismeModifie.txt';
             $class= 'Docalist\Biblio\Import\Prisme';
         } else {
             $path = $uploadDir . '/BDSP/Bdsp.csv';
