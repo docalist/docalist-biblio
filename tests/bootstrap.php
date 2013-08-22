3e4be82de7b9909cc3ba4e90f48f9249abdb840c
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-22 19:09:20 +0000

    Infrastructure de tests unitaires pour le plugin biblio.

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+// Environnement de test
+$GLOBALS['wp_tests_options'] = array(
+    'active_plugins' => array(
+        'docalist-0core/docalist-core.php',
+        'docalist-biblio/docalist-biblio.php'
+    ),
+);
+
+// wordpress-tests doit être dans le include_path de php
+// sinon, modifier le chemin d'accès ci-dessous
+require_once 'wordpress-tests/includes/bootstrap.php';
