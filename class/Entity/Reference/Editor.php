6308022b5d3738146d4ea09ecfba422a992a1390
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-22 19:03:40 +0000

    Entity/Reference/Editor : l'éditeur de l'ouvrage.

---
+++ class/Entity/Reference/Editor.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of a "Docalist Biblio" plugin.
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package Docalist
+ * @subpackage Biblio
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ * @version SVN: $Id$
+ */
+namespace Docalist\Biblio\Entity\Reference;
+
+use Docalist\Data\Entity\AbstractEntity;
+
+/**
+ * Editeur
+ *
+ * @property string $name
+ * @property string $city
+ * @property string $country
+ */
+class Editor extends AbstractEntity {
+
+    protected function loadSchema() {
+        // @formatter:off
+        return array(
+            'name' => array(
+                'label' => __('Nom', 'docalist-biblio'),
+                'description' => __("Nom de l'éditeur", 'docalist-biblio'),
+            ),
+            'city' => array(
+                'label' => __('Ville', 'docalist-biblio'),
+                'description' => __("Ville du siège social de l'éditeur", 'docalist-biblio'),
+            ),
+            'country' => array(
+                'label' => __('Pays', 'docalist-biblio'),
+                'description' => __("Pays du siège social de l'éditeur", 'docalist-biblio'),
+            ),
+        );
+        // @formatter:on
+    }
+}
