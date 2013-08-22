bea047b10aab9a0cdf7f24e424dae0e489fbb166
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-22 19:04:26 +0000

    Entity/Reference/DateBy

---
+++ class/Entity/Reference/DateBy.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of a "Docalist Biblio" plugin.
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package Docalist
+ * @subpackage Biblio
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ * @version SVN: $Id$
+ */
+namespace Docalist\Biblio\Entity\Reference;
+
+use Docalist\Data\Entity\AbstractEntity;
+
+/**
+ * Date de création ou de dernière modification et agent utilisateur.
+ *
+ * @property string $date
+ * @property string $by
+ */
+class DateBy extends AbstractEntity {
+
+    protected function loadSchema() {
+        // @formatter:off
+        return array(
+            'date' => array(
+                'label' => __('Le', 'docalist-biblio'),
+                'description' => __('Date', 'docalist-biblio'),
+            ),
+            'by' => array(
+                'label' => __('Par', 'docalist-biblio'),
+                'description' => __('Utilisateur', 'docalist-biblio'),
+            ),
+        );
+        // @formatter:on
+    }
+}
