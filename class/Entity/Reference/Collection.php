91467a8feb39bbe4f3d18460d542d9f7e9cec3da
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-22 19:04:13 +0000

    Entity/Reference/Collection : collection et numéro dans la collection.

---
+++ class/Entity/Reference/Collection.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of a "Docalist Biblio" plugin.
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package Docalist
+ * @subpackage Biblio
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ * @version SVN: $Id$
+ */
+namespace Docalist\Biblio\Entity\Reference;
+
+use Docalist\Data\Entity\AbstractEntity;
+
+/**
+ * Collection et numéro au sein de la collection.
+ *
+ * @property string $name
+ * @property string $number
+ */
+class Collection extends AbstractEntity {
+
+    protected function loadSchema() {
+        // @formatter:off
+        return array(
+            'name' => array(
+                'label' => __("Nom", 'docalist-biblio'),
+                'description' => __('Nom de la collection ou de la sous-collection.', 'docalist-biblio'),
+            ),
+            'number' => array(
+                'label' => __('Numéro', 'docalist-biblio'),
+                'description' => __('Numéro au sein de la collection ou de la sous-collection.', 'docalist-biblio'),
+            ),
+        );
+        // @formatter:on
+    }
+}
