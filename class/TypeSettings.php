4512ae0d76ea103ee830819e92a1810ae030f437
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-10-23 14:02:41 +0000

    Nouvelle classe TypeSettings (une entité) qui décrit un type de notice (nom, libellé, liste des champs, etc.)

---
+++ class/TypeSettings.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This file is part of the 'Docalist Biblio' plugin.
+ *
+ * Copyright (C) 2012, 2013 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Biblio
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     $Id$
+ */
+namespace Docalist\Biblio;
+
+use Docalist\Data\Entity\AbstractEntity;
+
+/**
+ * Paramètres d'un type de référence.
+ *
+ * Un type est essentiellement une liste de champs
+ *
+ * @property string $name Identifiant du type
+ * @property string $label Libellé du type
+ * @property string $description Description du type
+ * @property FieldSettings[] $fields Liste des champs de ce type
+ */
+class TypeSettings extends AbstractEntity {
+    protected function loadSchema() {
+        // @formatter:off
+        return array(
+            'name' => array(
+                'label' => __('Nom du type', 'docalist-biblio'),
+                'description' => __('Doit être un type enregistré (article, book, periodical website, etc.)', 'docalist-biblio'),
+            ),
+
+            'label' => array(
+                'label' => __('Libellé du type', 'docalist-biblio'),
+                'description' => __('Libellé utilisé pour désigner ce type', 'docalist-biblio'),
+            ),
+
+            'description' => array(
+                'label' => __('Description de ce type de référence, texte d\'intro, etc.', 'docalist-biblio'),
+                'description' => __('Détermine la page d\'accueil de la base et les urls des notices.', 'docalist-biblio'),
+            ),
+
+            'fields' => array(
+                'type' => 'Docalist\Biblio\FieldSettings*',
+                'label' => __('Liste des champs pour ce type', 'docalist-biblio'),
+                'description' => __('Liste des champs et paramètres de chaque champ.', 'docalist-biblio'),
+            ),
+        );
+        // @formatter:on
+    }
+}
