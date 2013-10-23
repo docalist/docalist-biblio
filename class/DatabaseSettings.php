68582f0cabcfd429930166a8d0671cf3081b7f92
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-10-23 14:06:36 +0000

    Adaptation de DatabaseSettings.
    Une base de données est maintenant définie par un nom, un slug, un libellé, une description, et une liste de types de notices (TypeSettings*)
    
    Ajout de la méthode typeNames() qui retourne les noms des types de notice gérés par la base.

--- class/DatabaseSettings.php
+++ class/DatabaseSettings.php
@@ -1,24 +1,31 @@
 <?php
 /**
- * This file is part of a "Docalist Biblio" plugin.
-*
-* For copyright and license information, please view the
-* LICENSE.txt file that was distributed with this source code.
-*
-* @package Docalist
-* @subpackage Biblio
-* @author Daniel Ménard <daniel.menard@laposte.net>
-* @version SVN: $Id$
-*/
+ * This file is part of the 'Docalist Biblio' plugin.
+ *
+ * Copyright (C) 2012, 2013 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Biblio
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     $Id$
+ */
 namespace Docalist\Biblio;
 
 use Docalist\Data\Entity\AbstractEntity;
 use DateTime;
 
 /**
+ * Paramètres d'une base de données.
+ *
+ * Une base est essentiellement une liste de types.
  *
  * @property string $name Identifiant de la base
  * @property string $label Libellé de la base
+ * @property string $slug Slug de la base de données
+ * @property TypeSettings[] $types Types de notices gérés dans cette base
  * @property string $creation Date de création de la base
  */
 class DatabaseSettings extends AbstractEntity {
@@ -27,7 +34,7 @@ class DatabaseSettings extends AbstractEntity {
         return array(
             'name' => array(
                 'label' => __('Nom de la base de données', 'docalist-biblio'),
-                'description' => __('Détermine le post-type (le préfixe dclref est ajouté). 14 caractères maxi, [a-z-]+. ', 'docalist-biblio'),
+                'description' => __('Détermine le post-type (le préfixe dclref est ajouté). 14 caractères maxi, [a-z-]+.', 'docalist-biblio'),
             ),
 
             'label' => array(
@@ -35,9 +42,19 @@ class DatabaseSettings extends AbstractEntity {
                 'description' => __('Libellé utilisé dans les menus, dans les écrans, etc.', 'docalist-biblio'),
             ),
 
+            'description' => array(
+                'label' => __('Description de la base de données', 'docalist-biblio'),
+                'description' => __('Description de la base.', 'docalist-biblio'),
+            ),
+
             'slug' => array(
                 'label' => __('Slug de la base de données', 'docalist-biblio'),
                 'description' => __('Détermine la page d\'accueil de la base et les urls des notices.', 'docalist-biblio'),
+            ),
+
+            'types' => array(
+                'type' => 'TypeSettings*',
+                'label' => __('Types de notices gérés dans cette base', 'docalist-biblio'),
             ),
 
             'creation' => array(
@@ -54,5 +71,15 @@ class DatabaseSettings extends AbstractEntity {
 
     private function now() {
         return new DateTime;
+    }
+
+    public function typeNames() {
+        $types = array();
+        foreach($this->types as $type) {
+            /* @var $type TypeSettings */
+            $types[$type->name] = $type->label;
+        }
+
+        return $types;
     }
 }
