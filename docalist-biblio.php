3e15a803467b74578cf72e42a465b5dcbde1a45a
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-10-23 13:48:14 +0000

    Typo.

--- docalist-biblio.php
+++ docalist-biblio.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * This file is part of a "Docalist Biblio" plugin.
+ * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012,2013 Daniel Ménard
+ * Copyright (C) 2012, 2013 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
