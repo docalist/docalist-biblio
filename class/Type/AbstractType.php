d69b9eb8ca3893bcb3c8acaaffb6d739f9284636
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-11-06 13:10:25 +0000

    Conversion des types en fichier de config, transfert des types dans /types.

--- class/Type/AbstractType.php
+++ class/Type/AbstractType.php
@@ -18,7 +18,6 @@ use Docalist\Biblio\Entity\Reference;
 use Docalist\Biblio\TypeSettings;
 use Docalist\Biblio\FieldSettings;
 
-use Docalist\Forms\Fragment;
 use Docalist\Forms\Input;
 use Docalist\Forms\Select;
 use Docalist\Forms\Table;
@@ -35,7 +34,12 @@ use Docalist\Forms\Checklist;
  * Classe de base pour les types de documents de docalist-biblio.
  */
 class AbstractType extends TypeSettings /* extends Reference */ {
+/*
     public function __construct(array $data = null) {
+//         echo '<pre>';
+//         debug_print_backtrace();
+//         var_export($data);
+//         echo '</pre>';
         if (isset($data['fields'])) {
             $schema = (new Reference())->schema();
 
@@ -56,7 +60,7 @@ class AbstractType extends TypeSettings /* extends Reference */ {
         }
         parent::__construct($data);
     }
-
+*/
     /**
      * Retourne le formulaire à utiliser pour créer une notice de ce type.
      *
@@ -75,13 +79,18 @@ class AbstractType extends TypeSettings /* extends Reference */ {
      *
      * @return Fragment[] Un tableau de la forme id metabox => form fragment
      */
+/*
     public function metaboxes() {
+        die('IN abstractType.metaboxes()');
         $metaboxes = array();
 
         $type = Utils::classname($this);
 
         $box = new Fragment();
         foreach($this->__get('fields') as $field) {
+            echo '<pre>';
+            var_export($field);
+            echo '</pre>';
             // Nouvelle métabox. Sauve la courante si non vide et crée une nouvelle
             if ($field->name === 'group') {
                 if (count($box->fields()) !== 0) {
@@ -102,264 +111,10 @@ class AbstractType extends TypeSettings /* extends Reference */ {
             $id = $type . '-' . $box->fields()[0]->name();
             $metaboxes[$id] = $box;
         }
-        // var_dump($metaboxes);
-        // die();
+        var_dump($metaboxes);
+         die();
         return $metaboxes;
 
     }
-
-    protected function createField(FieldSettings $def) {
-        $name = $def->name;
-        switch($name) {
-            case 'ref':
-                $field = new Input($name);
-                break;
-
-            case 'type':
-                $types = apply_filters('docalist_biblio_get_types', array()); // code => class
-                $types = array_keys($types);
-
-                $field = new Select($name);
-                $field->options($types);
-                break;
-
-            case 'genre':
-                $table = $def->table[0];
-                $field = new Select($name);
-                $field->options($this->tableOptions($table));
-                break;
-
-            case 'media':
-                $table = $def->table[0];
-                $field = new Select($name);
-                $field->options($this->tableOptions($table));
-                break;
-
-            case 'author':
-                $roles = $def->table[0];
-
-                $field = (new Table($name))->attribute('class', 'author');
-                $field->input('name')
-                      ->attribute('class', 'name');
-                $field->input('firstname')
-                      ->attribute('class', 'firstname');
-                $field->select('role')
-                      ->options($this->tableOptions($roles))
-                      ->attribute('class', 'role');
-
-                break;
-
-            case 'organisation':
-                $countries = $def->table[0];
-                $roles = $def->table[1];
-
-                $field = (new Table($name))->attribute('class', 'organisation');
-                $field->input('name')
-                      ->attribute('class', 'name');
-                $field->input('city')
-                      ->attribute('class', 'city');
-                $field->select('country')
-                      ->options($this->tableOptions($countries))
-                      ->attribute('class', 'country');
-                $field->select('role')
-                      ->options($this->tableOptions($roles))
-                      ->attribute('class', 'role');
-                break;
-
-            case 'title':
-                $field = new Input($name);
-                $field->addClass('large-text')->attribute('id', 'DocTitle');
-                break;
-
-            case 'othertitle':
-                $titles = $def->table[0];
-
-                $field = new Table($name);
-                $field->select('type')->options($this->tableOptions($titles));
-                $field->input('title');
-                break;
-
-            case 'translation':
-                $languages = $def->table[0];
-
-                $field = new Table($name);
-                $field->select('language')->options($this->tableOptions($languages));
-                $field->input('title');
-                break;
-
-            case 'date':
-                $field = new Input($name);
-
-                break;
-
-            case 'journal':
-                $field = new Input($name);
-                $field->attribute('class', 'large-text');
-
-                break;
-
-            case 'issn':
-                $field = new Input($name);
-
-                break;
-
-            case 'volume':
-                $field = new Input($name);
-
-                break;
-
-            case 'issue':
-                $field = new Input($name);
-
-                break;
-
-            case 'language':
-                $languages = $def->table[0];
-
-                $field = new Select($name);
-                $field->options($this->tableOptions($languages));
-
-                break;
-
-            case 'pagination':
-                $field = new Input($name);
-                break;
-
-            case 'format':
-                $field = new Input($name);
-                break;
-
-            case 'isbn':
-                $field = new Input($name);
-                break;
-
-            case 'editor':
-                $countries = $def->table[0];
-
-                $field = new Table($name);
-                $field->input('name');
-                $field->input('city');
-                $field->select('country')->options($this->tableOptions($countries));
-                break;
-
-            case 'edition':
-                $field = new Table($name);
-                $field->input('type');
-                $field->input('value');
-                break;
-
-            case 'collection':
-                $field = new Table($name);
-                $field->input('name');
-                $field->input('number');
-                break;
-
-            case 'event':
-                $field = new Table($name);
-                $field->input('title');
-                $field->input('date');
-                $field->input('place');
-                $field->input('number');
-                break;
-
-            case 'degree':
-                $field = new Table($name);
-                $field->input('title');
-                $field->input('level');
-                break;
-
-            case 'abstract':
-                $languages = $def->table[0];
-
-                $field = new Table($name);
-                $field->select('language')->options($this->tableOptions($languages));
-                $field->textarea('content');
-                break;
-
-            case 'topic':
-//                 $table = $def->table[0] ?: 'dcllanguage'; // TODO mettre la bonne table
-// var_dump($def->table->toArray());
-// die();
-                $field = new Table($name);
-                $field->select('type');//->options($def->table->toArray());
-                $field->input('term');
-                break;
-
-            case 'note':
-                $notes = $def->table[0];
-
-                $field = new Table($name);
-                $field->select('type')->options($this->tableOptions($notes));
-                $field->textarea('content');
-                break;
-
-            case 'link':
-                $links = $def->table[0];
-
-                $field = new Table($name);
-                $field->input('url');
-                $field->select('type')->options($this->tableOptions($links));
-                $field->input('label');
-                $field->input('date');
-                break;
-
-            case 'doi':
-                $field = new Input($name);
-                break;
-
-            case 'relations':
-                $relations = $def->table[0];
-
-                $field = new Table($name);
-                $field->select('type')->options($this->tableOptions($relations));
-                $field->input('ref');
-                break;
-
-            case 'owner':
-                $field = new Input($name);
-                break;
-
-            case 'creation':
-                $field = new Table($name);
-                $field->input('date');
-                $field->input('by');
-                break;
-
-            case 'lastupdate':
-                $field = new Table($name);
-                $field->input('date');
-                $field->input('by');
-                break;
-
-            case 'status':
-                $field = new Input($name);
-                break;
-
-            default:
-                throw new \Exception("Champ inconnu : '$name'");
-        }
-
-        $field->label($def->label)->description($def->description);
-
-        return $field;
-    }
-
-    protected function taxonomy($name) {
-        $terms = get_terms($name, array(
-            'hide_empty' => false,
-        ));
-
-        $result = array();
-        foreach ($terms as $term) {
-            $result[$term->slug] = $term->name;
-        }
-
-        return $result;
-    }
-
-    protected function tableOptions($table, $fields = 'code,label') {
-        /* @var $tableManager TableManager */
-        $tableManager = apply_filters('docalist_get_table_manager', null);
-        return $tableManager->get($table)->search($fields);
-    }
+*/
 }
