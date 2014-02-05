a73e84e9eb8d92f9ec17c4f7f53f96e2662f27da
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-02-05 16:38:11 +0000

    Nouveau type de table "topics" qui liste des vocabulaires disponibles pour l'indexation.

---
+++ tables/topics.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of the "Docalist Biblio" plugin.
+ *
+ * Copyright (C) 2012-2014 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Biblio
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+
+/*
+ * Liste des vocabulaires disponibles pour l'indexation.
+ *
+ * Ce type de table est destiné à être utilisé dans le champ "topic" et liste
+ * les différents vocabulaires.
+ *
+ * Par défaut, seul le vocabulaire "indexation libre" est disponible.
+ *
+ * La table contient les champs suivants :
+ * - code : nom de code du vocabulaire, doit être unique.
+ * - type : type de vocabulaire. Les valeurs possibles sont 'table' ou 'index'
+ * - source : source des entrées. Pour un vocabulaire de type "table", c'est le
+ *   nom de la table d'autorité à utiliser, pour un vocabulaire de type "index"
+ *   c'est le nom du champ elastic-search à utiliser.
+ * - label : libellé utilisé dans le champ topic.type
+ */
+
+return [
+    ['code', 'type', 'source', 'label'],
+
+    ['free', 'index', 'topic.free', __('Indexation libre', 'docalist-biblio')],
+];
