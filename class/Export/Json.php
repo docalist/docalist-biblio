d09747551b5a44c62d14cf42addb5d52d20094f5
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-06-10 15:25:42 +0000

    Expor/Json : un exporteur simple au format JSON.

---
+++ class/Export/Json.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of the 'Docalist Biblio' plugin.
+ *
+ * Copyright (C) 2012-2014 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Biblio
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     $Id$
+ */
+namespace Docalist\Biblio\Export;
+
+use Docalist\Biblio\Entity\Reference;
+use Docalist\Biblio\Entity\ReferenceIterator;
+
+/**
+ * Un exporteur au format JSON.
+ */
+class Json extends AbstractExporter {
+    public function mimeType() {
+        return 'application/json';
+    }
+
+    public function extension() {
+        return 'json';
+    }
+
+    public function export(ReferenceIterator $references) {
+        $options = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
+        $this->get('pretty') && $options .= JSON_PRETTY_PRINT;
+
+        /* @var $ref Reference */
+        foreach($references as $key => $ref) {
+            $data = $ref->toArray();
+            echo json_encode($data, $options);
+        }
+    }
+}
