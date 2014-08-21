424fbf1d7946298cb57951a81f19ec458eb6fb4a
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-08-21 09:12:56 +0000

    Création d'une entité Error pour Reference.

---
+++ class/Entity/Reference/Error.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of the 'Docalist Biblio' plugin.
+ *
+ * Copyright (C) 2012-2014 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Biblio
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     $Id$
+ */
+namespace Docalist\Biblio\Entity\Reference;
+
+use Docalist\Type\Object;
+use Docalist\Type\String;
+
+/**
+ * Une erreur.
+ *
+ * @property String $code
+ * @property String $value
+ * @property String $message
+ */
+class Error extends Object {
+    static protected function loadSchema() {
+        // @formatter:off
+        return [
+            'code',
+            'value',
+            'message'
+        ];
+        // @formatter:on
+    }
+}
