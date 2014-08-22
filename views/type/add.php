4364a74b4a39da9909b619db42401232ae19fcc4
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-08-22 05:05:57 +0000

    Adapte la vue "ajouter un type".

--- views/type/add.php
+++ views/type/add.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012, 2013 Daniel Ménard
+ * Copyright (C) 2012-2014 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -31,28 +31,23 @@ $back = $this->url('TypesList', $dbindex);
 ?>
 <div class="wrap">
     <?= screen_icon() ?>
-    <h2><?= sprintf(__('%s : ajouter un type de notice', 'docalist-biblio'), $database->label) ?></h2>
+    <h2><?= sprintf(__('%s : ajouter un type de notice', 'docalist-biblio'), $database->label()) ?></h2>
 
     <p class="description">
         <?= __('Sélectionnez les types de notice à ajouter dans la base :', 'docalist-biblio') ?>
     </p>
 
     <form method="POST" action="<?=$this->url('TypeAdd', $dbindex) ?>">
-        <ul class="ul-disc">
-            <?php
-            foreach($types as $type) :
-                /* @var $type TypeSettings */
-
-                $add = $this->url('TypeAdd', $dbindex, $type->name);
-                ?>
+        <ul>
+            <?php foreach($types as $type) : /* @var $type TypeSettings */ ?>
                 <li>
                     <h3>
                         <label>
-                            <input type="checkbox" name="name[]" value="<?= $type->name ?>" />
-                            <?= $type->label ?> (<?= $type->name ?>)
+                            <input type="checkbox" name="name[]" value="<?= $type->name() ?>" />
+                            <?= $type->label() ?> (<?= $type->name() ?>)
                         </label>
                     </h3>
-                    <p class="description"><?= $type->description ?></p>
+                    <p class="description"><?= $type->description() ?></p>
                 </li>
             <?php endforeach ?>
         </ul>
