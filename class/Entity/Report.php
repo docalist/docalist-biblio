33bbb61337bfce7e586b4f08327d6aed4247f75a
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-08-30 10:51:55 +0000

    Création du type Report.

---
+++ class/Entity/Report.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * This file is part of the 'Docalist Biblio' plugin.
+ *
+ * Copyright (C) 2012-2014 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Biblio
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     $Id$
+ */
+namespace Docalist\Biblio\Entity;
+
+/**
+ * Un rapport.
+ *
+ * - Un rapport est un document analysant et évaluant un fonctionnement ou une
+ *   activité, dans un ou plusieurs domaines, pour une période donnée.
+ *   @see http://fr.wikipedia.org/wiki/Reporting
+ *
+ * - A report or account is any informational work made with the specific
+ *   intention of relaying information or recounting certain events in a
+ *   widely presentable form. Written reports are documents which present
+ *   focused, salient content to a specific audience. Reports are often used
+ *   to display the result of an experiment, investigation, or inquiry.
+ *   The audience may be public or private, an individual or the public in
+ *   general. Reports are used in government, business, education, science,
+ *   and other fields.
+ *   @see http://en.wikipedia.org/wiki/Report
+ *
+ * Principales caractéristiques :
+ * - pas d'éditeur
+ * - pas d'isbn
+ */
+
+// TODO : ne pas appeller ça "rapport". Ce que ça désigne, c'est une monographie non éditée (pas d'isbn)
+class Report extends Reference {
+    static protected function loadSchema() {
+        $fields = parent::loadSchema()['fields'];
+
+        // @formatter:off
+        return [
+            'name' => 'report',
+            'label' => __('Rapport', 'docalist-biblio'),
+            'description' => __('Un rapport d\'activité ou une étude non publiée', 'docalist-biblio'),
+            'fields' => [
+                // Type, Genre, Media
+                'group1' => ['type' => 'Docalist\Biblio\Entity\Reference\Group', 'label' => 'Nature du document'],
+                $fields['genre'],
+                $fields['media'],
+
+                // Title, OtherTitle, Translation
+                'group2' => ['type' => 'Docalist\Biblio\Entity\Reference\Group', 'label' => 'Titres'],
+                $fields['title'],
+                $fields['translation'],
+
+                // Author, Organisation
+                'group3' => ['type' => 'Docalist\Biblio\Entity\Reference\Group', 'label' => 'Auteurs'],
+                $fields['organisation'],
+                $fields['author'],
+
+                // Date / Language / Pagination / Format
+                'group4' => ['type' => 'Docalist\Biblio\Entity\Reference\Group', 'label' => 'Informations bibliographiques'],
+                $fields['date'],
+                $fields['language'],
+                $fields['number'],
+                $fields['extent'],
+                $fields['format'],
+
+                // Topic / Abstract / Note
+                'group5' => ['type' => 'Docalist\Biblio\Entity\Reference\Group', 'label' => 'Indexation et résumé'],
+                $fields['topic'],
+                $fields['content'],
+
+                // // Liens et relations
+                'group6' => ['type' => 'Docalist\Biblio\Entity\Reference\Group', 'label' => 'Liens et relations'],
+                $fields['link'],
+                $fields['relation'],
+
+                // Ref / Owner / Creation / Lastupdate
+                'group7' => ['type' => 'Docalist\Biblio\Entity\Reference\Group', 'label' => 'Informations de gestion'],
+                $fields['type'],
+                $fields['ref'],
+                $fields['owner'],
+            ]
+        ];
+        // @formatter:on
+    }
+}
