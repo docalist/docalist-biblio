c75530a0d0d178d512221041507f81c4dc89a59e
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-09-01 09:00:53 +0000

    vue type/edit : les types sont à nouveau des objets, donc on n'a pas besoin de répéter les label/description.

--- views/type/edit.php
+++ views/type/edit.php
@@ -15,20 +15,20 @@
 namespace Docalist\Biblio\Views;
 
 use Docalist\Biblio\DatabaseSettings;
-use Docalist\Schema\Schema;
 use Docalist\Forms\Form;
+use Docalist\Biblio\TypeSettings;
 
 /**
  * Edite les paramètres d'un type.
  *
  * @param DatabaseSettings $database La base à éditer.
  * @param int $dbindex L'index de la base.
- * @param Schema $type Le type à éditer.
+ * @param TypeSettings $type Le type à éditer.
  * @param int $typeindex L'index du type.
  */
 
 /* @var $database DatabaseSettings */
-/* @var $type Schema */
+/* @var $type TypeSettings */
 
 ?>
 <div class="wrap">
@@ -44,17 +44,12 @@ use Docalist\Forms\Form;
         //$form->input('name')->attribute('class', 'regular-text');
 
         $form->input('label')
-             ->attribute('class', 'regular-text')
-             ->label(__('Libellé', 'docalist-biblio'))
-             ->description(__('Libellé utilisé pour désigner ce type', 'docalist-biblio'));
-
+             ->attribute('class', 'regular-text');
         $form->textarea('description')
              ->attribute('rows', '2')
-             ->attribute('class', 'large-text')
-             ->label(__('Description', 'docalist-biblio'))
-             ->description(__('Description de ce type de référence, texte d\'intro, etc.', 'docalist-biblio'));
+             ->attribute('class', 'large-text');
         $form->submit(__('Enregistrer les modifications', 'docalist-biblio'));
 
-        $form->bind($type->value())->render('wordpress');
+        $form->bind($type)->render('wordpress');
     ?>
 </div>
