aa6e1b0a3795d10463bad1c48de9c4d6cc5025de
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-09-04 17:39:23 +0000

    Introduit le ttpe StringTable : un champ texte avec une table d'autorité associée.

---
+++ class/Type/StringTable.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of the 'Docalist Biblio' plugin.
+ *
+ * Copyright (C) 2012-2014 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Biblio
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     $Id$
+ */
+namespace Docalist\Biblio\Type;
+
+use Docalist\Biblio\Type\String;
+use Docalist\Schema\Field;
+
+/**
+ * Un champ texte avec une table associée (genre, media, languages, format, etc.)
+ */
+class StringTable extends String {
+    public function format() {
+        return $this->label();
+    }
+
+    /**
+     * Retourne le libellé associé à la valeur du champ.
+     *
+     * @return string
+     */
+    public function label() {
+        return $this->openTable()->find('label', sprintf('code="%s"', $this->value))
+               ?: $this->value;
+    }
+}
