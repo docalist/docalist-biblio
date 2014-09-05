0b1d16e2329fc8eefb023caadcebe52d35a35d3b
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-09-05 16:59:03 +0000

    Extent est maintenant un multifield, 4 formats comme pour Number.

--- class/Field/Extent.php
+++ class/Field/Extent.php
@@ -14,7 +14,7 @@
  */
 namespace Docalist\Biblio\Field;
 
-use Docalist\Biblio\Type\Object;
+use Docalist\Biblio\Type\MultiField;
 
 /**
  * Etendue du document : pagination, nombre de pages, durée en minutes, etc.
@@ -22,7 +22,9 @@ use Docalist\Biblio\Type\Object;
  * @property String $type
  * @property String $value
  */
-class Extent extends Object {
+class Extent extends MultiField {
+    static protected $groupkey = 'type';
+
     static protected function loadSchema() {
         // @formatter:off
         return [
@@ -41,4 +43,32 @@ class Extent extends Object {
     }
 
     // map : champ non indexé
+
+    protected static function initFormats() {
+        self::registerFormat('format', "Format indiqué dans la table d'autorité", function(Extent $Extent, Extents $parent) {
+            $format = $parent->lookup($Extent->type(), false, 'code', 'format');
+            return trim(sprintf($format, $Extent->__get('value')->value()));
+        });
+
+        self::registerFormat('label', "Libellé indiqué dans la table suivi de la valeur", function(Extent $Extent, Extents $parent) {
+            $label = $parent->lookup($Extent->type());
+            return trim($label . ' ' . $Extent->__get('value')->value());
+        });
+
+        self::registerFormat('v', 'Valeur uniquement, sans aucune mention', function(Extent $Extent) {
+            return $Extent->__get('value')->value();
+        });
+
+        self::registerFormat('v (t)', 'Valeur suivie du type entre parenthèses', function(Extent $Extent, Extents $parent) {
+            $result = $Extent->__get('value')->value();
+            if (isset($Extent->type)) {
+                $result && $result .= ' '; // espace insécable avant '('
+                $result .= '(' . $parent->lookup($Extent->type()) . ')';
+            }
+
+            return $result;
+        });
+
+        // TODO : return Extent exemple ou array(Extent, Extent...)
+    }
 }
