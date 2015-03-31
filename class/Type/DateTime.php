6bc7ec431b8445f369afaf14187d249feb86af44
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-03-31 18:16:16 +0000

    DateTime : corrections / simplification
    - Supprime les méthodes ESmapping et map : on ne veut pas que tous les champs DateTime soient systématiquement indexés. C'est à chaque champ de le dire, pas à la classe de base.
    - Supprime la méthode editForm() : pour le moment, elle est strictement identique à celle de la classe parente (String), donc inutile de répéter (par la suite, on pourra implémenter un date picker par exemple).
    - Supprime les use inutile.

--- class/Type/DateTime.php
+++ class/Type/DateTime.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -14,33 +14,12 @@
  */
 namespace Docalist\Biblio\Type;
 
-use Docalist\Biblio\Type\String;
-use Docalist\Forms\Input;
-use Docalist\Schema\Field;
-
 /**
  * Une date/heure stockée sous forme de chaine au format 'yyyy-MM-dd HH:mm:ss'.
  *
  * Exemple : "2014-09-02 11:19:24"
- *
- * Utilisé pour les champs creation et lastupdate.
  */
 class DateTime extends String {
-    public function editForm() {
-        $field = new Input($this->schema->name());
-
-        return $field;
-    }
-
-    public function map(array & $doc) {
-        $doc[$this->schema->name()] = $this->value();
-    }
-
-    public static function ESmapping(array & $mappings, Field $schema = null) {
-        $mappings['properties'][$schema->name()] = [
-            'type' => 'date',
-            'format' => 'yyyy-MM-dd HH:mm:ss',
-            'ignore_malformed' => true,
-        ];
-    }
+//     public function editForm() { // @todo : générer un date/time picker
+//     }
 }
