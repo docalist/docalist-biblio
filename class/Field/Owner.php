c41bea3f980c9a411d8069deb93727b242846b75
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-04-01 07:02:38 +0000

    Owner : mapping() / map().

--- class/Field/Owner.php
+++ class/Field/Owner.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -15,17 +15,17 @@
 namespace Docalist\Biblio\Field;
 
 use Docalist\Biblio\Type\String;
-use Docalist\Schema\Field;
+use Docalist\Search\MappingBuilder;
 
 /**
  * Un producteur.
  */
 class Owner extends String {
-    public function map(array & $doc) {
-        $doc['owner'] = $this->value();
+    public function mapping(MappingBuilder $mapping) {
+        $mapping->field('owner')->text()->filter();
     }
 
-    public static function ESmapping(array & $mappings, Field $schema) {
-        $mappings['properties']['owner'] = self::stdIndexAndFilter();
+    public function map(array & $document) {
+        $document['owner'] = $this->value();
     }
 }
