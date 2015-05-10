33bcbee68d084e3b979dde88cb62506018dea2d4
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-05-10 05:37:38 +0000

    Group : permet aux groupes d'une grille d'avoir une capability.

--- class/Type/Group.php
+++ class/Type/Group.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of a "Docalist Core" plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -54,6 +54,12 @@ class Group extends \Docalist\Type\Any implements BiblioField {
              ->label(__('Titre du groupe', 'docalist-biblio'))
              ->description(__("C'est le titre qui sera affiché dans la barre de titre du groupe et dans les options de l'écran de saisie. Valeur par défaut : type de la notice", 'docalist-biblio'));
 
+        $form->input('capability')
+            ->attribute('id', $name . '-capability')
+            ->attribute('class', 'capability regular-text')
+            ->label(__('Droit requis', 'docalist-biblio'))
+            ->description(__("Droit requis pour afficher ce groupe de champs. Ce groupe (et tous les champs qu'il contient) sera masqué si l'utilisateur ne dispose pas du droit indiqué. Si vous laissez vide, aucun test ne sera effectué.", 'docalist-biblio'));
+
         $form->textarea('description')
              ->attribute('id', $name . '-description')
              ->attribute('class', 'description large-text')
@@ -89,6 +95,13 @@ class Group extends \Docalist\Type\Any implements BiblioField {
              ->attribute('class', 'label regular-text')
              ->label(__('Nom du groupe', 'docalist-biblio'))
              ->description(__("Ce texte n'est pas affiché dans les notices, il sert uniquement à distinguer les différents groupes.", 'docalist-biblio'));
+
+        $form->input('capability')
+            ->attribute('id', $name . '-capability')
+            ->attribute('class', 'capability regular-text')
+            ->label(__('Droit requis', 'docalist-biblio'))
+            ->description(__("Droit requis pour afficher ce groupe de champs. Ce groupe (et tous les champs qu'il contient) sera masqué si l'utilisateur ne dispose pas du droit indiqué. Si vous laissez vide, aucun test ne sera effectué.", 'docalist-biblio'));
+
 /*
         $form->textarea('setup')
              ->attribute('class', 'setup code large-text')
