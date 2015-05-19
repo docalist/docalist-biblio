64cf4d756934ea624685c959282c193e905183a6
Daniel Ménard <daniel.menard@laposte.net>
2015-05-19 17:08:01 +0200

    Widget : récupère la page "export" qui figure dans les settings.

--- class/Export/ExportWidget.php
+++ class/Export/ExportWidget.php
@@ -88,7 +88,7 @@ class ExportWidget extends WP_Widget {
         echo '<ul>';
 
         // Détermine l'url de la page "export"
-        $exportPage = '/export/';
+        $exportPage = get_permalink(docalist('docalist-biblio-export')->settings()->exportpage());
 
         // Lien "Exporter"
         $label = $settings['file'];
