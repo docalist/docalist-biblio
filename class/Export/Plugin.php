78e8e7681d342424ae9177e11d21dcb7f89fb5be
Daniel Ménard <daniel.menard@laposte.net>
2015-05-20 00:11:11 +0200

    Plugin : version initiale de la gestion du formulaire

--- class/Export/Plugin.php
+++ class/Export/Plugin.php
@@ -16,6 +16,8 @@ namespace Docalist\Biblio\Export;
 use WP_Query;
 use Docalist\Search\SearchRequest;
 use Docalist\Http\ViewResponse;
+use Docalist\Search\SearchResults;
+use RuntimeException;
 
 /**
  * Extension pour Docalist Biblio : génère des fichiers d'export et des
@@ -57,7 +59,7 @@ class Plugin {
         add_filter('docalist_search_create_request', function(SearchRequest $request = null, WP_Query $query) {
             // Stocke la dernière requête exécutée par docalist-search
             if ($request && $request->isSearch()) {
-                set_transient(self::TRANSIENT, $request, 600); // 10min
+                set_transient(self::TRANSIENT, $request, 3600); // 10min
             }
 
             // Déclenche l'export si on est sur la page "export"
@@ -125,13 +127,113 @@ class Plugin {
             return $content;
         }
 
-        $request = get_transient(self::TRANSIENT);
+        // Affiche un message si on n'a aucune requête en cours
+        $request = get_transient(self::TRANSIENT); /* @var $request SearchRequest */
         if ($request === false) {
             return $this->view('docalist-biblio-export:norequest');
         }
 
-        var_dump($request);
-        return 'formulaire export';
+        // Exécute la requête
+        $request->facet('_type', 100);
+        $results = $request->execute('count'); /* @var $results SearchResults */
+
+        // Affiche un message si on a aucune réponse
+        if ($results->total() === 0) {
+            return $this->view('docalist-biblio-export:nohits');
+        }
+
+        // Détermine la liste des types de notices qu'on va exporter
+        $countByType = $types = [];
+        foreach($results->facet('_type')->terms as $term) {
+            $types[] = $term->term;
+            $label = apply_filters('docalist_search_get_facet_label', $term->term, '_type');
+            $countByType[$label] = $term->count;
+        }
+
+        // Récupère la liste des formats d'export possibles
+        $formats = $this->formats($types);
+        if (empty($formats)) {
+            return $this->view('docalist-biblio-export:noformat', [
+                'types' => $countByType,
+                'total' => $results->total(),
+                'max' => 100,
+            ]);
+        }
+
+        // Initialise les options
+        $mail = isset($_REQUEST['mail']) && $_REQUEST['mail'] === '1';
+        $zip  = isset($_REQUEST['zip']) && $_REQUEST['zip'] === '1';
+        $format = isset($_REQUEST['format']) ? $_REQUEST['format'] : null;
+        !isset($formats[$format]) && $format = null; // vérifie que le format existe
+        is_null($format) && $format=key($formats);
+
+        // Affiche le texte d'introduction (nb de hits, types, limites, etc.)
+        $content = $this->view('docalist-biblio-export:form', [
+            'types' => $countByType,
+            'total' => $results->total(),
+            'max' => 100,
+            'formats' => $formats,
+            'format' => is_null($format) ? key($formats) : $format, // le premier par défaut
+            'mail' => $mail,
+            'zip' => $zip,
+        ]);
+
+        return $content;
+    }
+
+    protected function formats(array $types) {
+        // Dans une table 'export-formats', on aura :
+        // 'formats' => liste des formats définis = ['name', 'converter', 'converter-settings', 'exporter', 'exporter-settings']
+        // Dans les settings, on aura pour chaque type indexé par docalist-search :
+        // 'formats' => liste des formats autorisés pour ce type = ['format1', 'format2', ...]
+
+        // Récupère la liste des formats d'export définis
+        // TODO : depuis la table
+        $formats = apply_filters('docalist_biblio_get_export_formats', []);
+        if (empty($formats)) {
+            throw new \RuntimeException(__("Aucun format d'export disponible", 'docalist-biblio'));
+        }
+
+        // on récupère un tableau de la forme
+        // $formats['code-format'] = ['label' => ..., 'converter' => ..., 'exporter' => ... ]
+
+        // Liste des formats autorisés pour chaque type
+        // TODO : depuis les settings
+        $formatsByType = [
+            'post' => [
+                'docalist-json-pretty',
+                'docalist-xml',
+                'docalist-xml-pretty',
+            ],
+            'page' => [
+                'docalist-json-pretty',
+//                 'docalist-xml-pretty',
+            ],
+            'dclrefprisme' => [
+                'docalist-json',
+                'docalist-json-pretty',
+                'docalist-xml',
+                'docalist-xml-pretty',
+                'prisme2014-json',
+                'prisme2014-json-pretty',
+                'prisme2014-uppercase-json',
+                'prisme2014-uppercase-json-pretty',
+                'prisme2014-xml',
+                'prisme2014-xml-pretty',
+                'prisme2014-uppercase-xml',
+                'prisme2014-uppercase-xml-pretty'
+            ],
+        ];
+
+        foreach($types as $type) {
+            if (! isset($formatsByType[$type])) {
+                $formats = [];
+                break;
+            }
+            $formats = array_intersect_key($formats, array_flip($formatsByType[$type]));
+        }
+
+        return $formats;
     }
 
     /**
