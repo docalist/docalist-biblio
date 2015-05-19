4f27e6066f5865b148704001fdae49159061b495
Daniel Ménard <daniel.menard@laposte.net>
2015-05-19 19:32:02 +0200

    Plugin : mécanisme de base
    - stocke la dernière requête exécutée dans un transient.
    - lance l'export quand on est sur la page panier.
    - génère uen vue "norequest" si on n'a pas de requête en cours.

--- class/Export/Plugin.php
+++ class/Export/Plugin.php
@@ -13,6 +13,10 @@
  */
 namespace Docalist\Biblio\Export;
 
+use WP_Query;
+use Docalist\Search\SearchRequest;
+use Docalist\Http\ViewResponse;
+
 /**
  * Extension pour Docalist Biblio : génère des fichiers d'export et des
  * bibliographies.
@@ -24,6 +28,14 @@ class Plugin {
      * @var Settings
      */
     protected $settings;
+
+    /**
+     * Nom du transient utilisé pour stocker la dernière requête docalist-search
+     * exécutée.
+     *
+     * @var string
+     */
+    const TRANSIENT = 'docalist-biblio-export-last-request';
 
     public function __construct() {
         // Charge les fichiers de traduction du plugin
@@ -42,6 +54,20 @@ class Plugin {
             register_widget('Docalist\Biblio\Export\ExportWidget');
         });
 
+        add_filter('docalist_search_create_request', function(SearchRequest $request = null, WP_Query $query) {
+            // Stocke la dernière requête exécutée par docalist-search
+            if ($request && $request->isSearch()) {
+                set_transient(self::TRANSIENT, $request, 600); // 10min
+            }
+
+            // Déclenche l'export si on est sur la page "export"
+            if ($query->is_main_query() && $query->is_page && $query->get_queried_object_id() === $this->exportPage()) {
+                $this->export();
+            }
+
+            return $request;
+        }, 9999, 2); // priorité haute pour être le dernier
+
         // Déclare nos assets
 //         require_once dirname(__DIR__) . '/assets/register.php';
     }
@@ -53,5 +79,71 @@ class Plugin {
      */
     public function settings() {
         return $this->settings;
+    }
+
+    /**
+     * Retourne l'ID de la page "export" indiquée dans les paramètres du plugin.
+     *
+     * @return int
+     */
+    public function exportPage() {
+        return $this->settings->exportpage();
+    }
+
+    /**
+     * Gère l'export.
+     *
+     * Teste si on a tous les paramètres requis, affiche le formulaire si ce
+     * n'est pas le cas, lance l'export sinon.
+     */
+    public function export() {
+        $ok = isset($_REQUEST['go']);
+        if ($ok) {
+            die('export généré');
+        }
+        add_filter('the_content', [$this, 'injectForm'], 9999); // priorité très haute pour ignorer wp_autop et cie.
+        add_filter('the_excerpt', [$this, 'injectForm'], 9999); // priorité très haute pour ignorer wp_autop et cie.
+
+    }
+
+    /**
+     * Injecte le formulaire d'export dans le contenu ou l'extrait passé en
+     * paramètre.
+     *
+     * Cete méthode est appellée via the_content()/the_excerpt().
+     *
+     * @param string $content Le contenu de la page "export".
+     *
+     * @return string Le contenu de la page "export" si on n'a rien à exporter,
+     * le formulaire d'export sinon.
+     */
+    public function injectForm($content) {
+        global $post;
+
+        // Vérifie que c'est bien notre page
+        if ($post->ID !== $this->exportPage()) {
+            return $content;
+        }
+
+        $request = get_transient(self::TRANSIENT);
+        if ($request === false) {
+            return $this->view('docalist-biblio-export:norequest');
+        }
+
+        var_dump($request);
+        return 'formulaire export';
+    }
+
+    /**
+     * Exécute la vue indiquée et retourne le contenu généré
+     * @param string $view Nom de la vue.
+     * @param array $viewArgs Paramètres à passer à la vue.
+     */
+    protected function view($view, array $viewArgs = []){
+        !isset($viewArgs['this']) && $viewArgs['this'] = $this;
+
+        $view = new ViewResponse($view, $viewArgs);
+
+        return $view->getContent();
     }
 }
