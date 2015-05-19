17cec5e96b9037093978dde687b59ab4e96a5ecd
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-05-19 08:18:57 +0000

    database/edit : restructure le formulaire en insérant des sous-titres entre les différentes sections et revoit la description des champs.

--- views/database/edit.php
+++ views/database/edit.php
@@ -55,32 +55,61 @@ use Docalist\Utils;
             }
         }
 
-        $homePageDescription = sprintf(
-            __("
-                Choisissez la page d'accueil de votre base.
-                Les références auront un permalien de la forme <code>%s/votre/page/12345/</code>.",
-                'docalist-biblio'
-            ),
-            home_url()
-        );
-
         $form = new Form('', 'post');
-        $form->input('name')->attribute('class', 'regular-text');
-        $form->select('homepage')->options(pagesList())->firstOption(false)->description($homePageDescription);
-        $form->input('label')->attribute('class', 'regular-text');
-        $form->textarea('description')->attribute('rows', 2)->attribute('class', 'large-text');
+
+        $form->tag('h3.title', __('Paramètres généraux', 'docalist-biblio'))
+             ->description(__('Options de publication de votre base de données.', 'docalist-biblio'));
+        $form->input('name')
+             ->attribute('class', 'regular-text')
+             ->description(__('Nom de code interne de la base de données, de 1 à 14 caractères, lettres minuscules, chiffres et tiret autorisés.', 'docalist-biblio'));
+        $form->select('homepage')
+             ->options(pagesList())
+             ->firstOption(false)
+             ->description(__("Choisissez la page d'accueil de votre base. Les références auront un permalien de la forme <code>/votre/page/12345/</code>.", 'docalist-biblio'));
+
+        $form->tag('h3.title', __('Fonctionnalités', 'docalist-biblio'))
+             ->description(__('Options et fonctionnalités disponibles pour cette base.', 'docalist-biblio'));
         $form->checkbox('thumbnail');
         $form->checkbox('revisions');
         $form->checkbox('comments');
-        $form
-            ->select('stemming')
-            ->attribute('class', 'regular-text')
-            ->firstOption(__('(Pas de stemming)', 'docalist-biblio'))
-            ->options($analyzers);
-        $form->input('icon')->attribute('class', 'medium-text');
-        $form->textarea('notes')->attribute('rows', 10)->attribute('class', 'large-text');
-        $form->input('creation')->attribute('disabled', true);
-        $form->input('lastupdate')->attribute('disabled', true);
+
+        $form->tag('h3.title', __('Indexation docalist-search', 'docalist-biblio'))
+             ->description(__("Options d'indexation dans le moteur de recherche.", 'docalist-biblio'));
+        $form->select('stemming')
+             ->attribute('class', 'regular-text')
+             ->firstOption(__('(Pas de stemming)', 'docalist-biblio'))
+             ->options($analyzers);
+
+        $form->tag('h3.title', __('Intégration dans WordPress', 'docalist-biblio'))
+             ->description(__("Apparence de cette base dans le back-office de WordPress.", 'docalist-biblio'));
+        $form->input('icon')
+             ->attribute('class', 'medium-text')
+             ->description(sprintf(
+                __('Icône à utiliser dans le menu de WordPress. Par exemple %s pour obtenir l\'icône %s.<br />
+                    Pour choisir une icône, allez sur le site %s, faites votre voix et recopiez le nom de l\'icône.<br />
+                    Remarque : vous pouvez également indiquer l\'url complète d\'une image, mais dans ce cas celle-ci ne s\'adaptera pas automatiquement au back-office de WordPress.',
+                    'docalist-biblio'),
+                '<code>dashicons-book</code>',
+                '<span class="dashicons dashicons-book"></span>',
+                '<a href="https://developer.wordpress.org/resource/dashicons/#book" target="_blank">WordPress dashicons</a>'
+            ));
+        $form->input('label')
+             ->attribute('class', 'regular-text');
+        $form->textarea('description')
+             ->attribute('rows', 2)
+             ->attribute('class', 'large-text');
+
+        $form->tag('h3.title', __('Autres informations', 'docalist-biblio'))
+             ->description(__('Informations pour vous.', 'docalist-biblio'));
+        $form->input('creation')
+             ->attribute('disabled', true);
+        $form->input('lastupdate')
+             ->attribute('disabled', true);
+        $form->textarea('notes')
+             ->attribute('rows', 10)
+             ->attribute('class', 'large-text')
+             ->description(__("Vous pouvez utiliser cette zone pour stocker toute information qui vous est utile : historique, modifications apportées, etc.", 'docalist-biblio'));
+
         $form->submit(__('Enregistrer les modifications', 'docalist-biblio'));
 
         $assets=$form->assets();
