e6c71cfda3da5858100be7f62ca41c7f4fe90884
Daniel Ménard <daniel.menard@laposte.net>
2015-08-31 15:29:42 +0200

    Type Meeting : supprime 'event' de l'affichage court
    Le champ event figurait dans la grille 'edit' alors qu'il n'existe pas
    dans la grille de base.
    
    docalist/docalist#305

--- class/Reference/Meeting.php
+++ class/Reference/Meeting.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -144,7 +144,7 @@ class Meeting extends Reference {
                 // Title, OtherTitle, Translation
                 'group2' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Titres'],
                 'title',
-                'event',
+//              'event',
                 'translation',
 //              'othertitle',
 
