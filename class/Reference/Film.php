f328b5808a9b0801fcafebba8e08ebba7dc2ff46
Daniel Ménard <daniel.menard@laposte.net>
2017-06-09 17:08:34 +0200

    Film : ré-écriture schéma avec unused + getEditGrid()

--- class/Reference/Film.php
+++ class/Reference/Film.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -23,145 +23,142 @@ use Docalist\Schema\Schema;
  *
  * - Film distribué : possède un diffuseur, etc. comme un livre.
  */
-class Film extends Reference {
-    static public function loadSchema() {
-        // Récupère les champs d'une référence standard
-        $fields = parent::loadSchema()['fields'];
-
-        // Supprime les champs qu'on n'utilise pas
-        unset($fields['journal']);
-
-        // Personnalise les tables, les libellés, les description, etc.
-        $fields['title']['label'] = __('Titre du film', 'docalist-biblio');
-        $fields['title']['description'] = __('Titre exact du film.', 'docalist-biblio');
-
-        $fields['genre']['label'] = __('Genre', 'docalist-biblio');
-        $fields['genre']['description'] = __("Type de film : documentaire, fiction, reportage, clip, congrès, film d'animation, débat, etc.", 'docalist-biblio');
-
-        $fields['media']['label'] = __('Support de diffusion', 'docalist-biblio');
-        $fields['media']['description'] = __('Support physique utilisé pour la diffusion du film : dvd, vhs, en ligne...', 'docalist-biblio');
-
-        $fields['extent']['label'] = __('Durée du film', 'docalist-biblio');
-
-        $fields['author']['label'] = __('Personnes', 'docalist-biblio');
-        $fields['author']['description'] = __("Liste des personnes qui ont contribué à l'élaboration du film : producteur, réalisateur, scénariste, acteurs...", 'docalist-biblio');
-
-        $fields['organisation']['description'] = __('Liste des organismes qui ont contribué au film : organisme considéré comme auteur, organisme commanditaire, financeur, producteur...', 'docalist-biblio');
-
-//      $fields['othertitle']['label'] = __('Autres titres du film', 'docalist-biblio');
-        $fields['othertitle']['description'] = __('Autres titres du film : variante, sous-titre, etc.', 'docalist-biblio');
-
-        $fields['translation']['label'] = __("Titre dans d'autres langues", 'docalist-biblio');
-        $fields['translation']['description'] = __("Titre du film dans d'autres langues : titre original, traduction du titre dans une autre langue...", 'docalist-biblio');
-
-        $fields['date']['label'] = __('Date du film', 'docalist-biblio');
-        $fields['date']['description'] = __("Dates au format <code>AAAAMMJJ</code> : date de sortie, date d'enregistrement, date de première diffusion...", 'docalist-biblio');
-
-        $fields['language']['description'] = __('Langue du film et langues des pistes audio disponibles.', 'docalist-biblio');
-
-        $fields['format']['label'] = __('Autres caractéristiques', 'docalist-biblio');
-        $fields['format']['description'] = __('Autres informations sur le film : format de la vidéo, sous-titres disponibles, couleur/n&b, VO/VF, bonus, etc.', 'docalist-biblio');
-
-        $fields['number']['description'] = __("Utilisez les zones ci-dessus pour indiquer les numéros éventuels associés au film : numéro de dépôt légal, visa d'exploitation, numéro de saison et d'épisode, numéro d'opus, etc.", 'docalist-biblio');
-
-        $fields['editor']['label'] = __('Editeur ou distributeur', 'docalist-biblio');
-        $fields['editor']['description'] = __("Organismes délégués par l'auteur pour assurer l'édition, la diffusion, la distribution ou la commercialisation du film.", 'docalist-biblio');
-
-        $fields['edition']['label'] = __('Mentions de version', 'docalist-biblio');
-        $fields['edition']['description'] = __("Si le film a fait l'objet de plusieurs versions, indiquez ici les informations permettant d'identifier la version cataloguée (exemple : version longue).", 'docalist-biblio');
-
-        $fields['collection']['label'] = __('Série ou collection', 'docalist-biblio');
-        $fields['collection']['description'] = __("Si le film fait partie d'une série ou d'une collection, vous pouvez l'indiquer ici et mentionner le numéro éventuel de ce film au sein de la série.", 'docalist-biblio');
-
-        $fields['event']['label'] = __('Quoi, quand, où ?', 'docalist-biblio');
-        $fields['event']['description'] = __("Si le film a été réalisé à l'occasion d'un événement (un colloque, un concert, une assemblée, etc.), indiquez les informations disponibles dans les zones ci-dessus.", 'docalist-biblio');
-
-        $fields['content']['description'] = __("Informations relatives au contenu du film : synopsis, résumé, extrait d'une critique, ce qu'en dit la presse, remarques sur le financement, avertissement sur le contenu, objectifs pédagogiques, interview du réalisateur...", 'docalist-biblio');
-
-        $fields['link']['description'] = __('Liens relatifs au film (site dédié, extrait, bande annonce...) ou à ses auteurs (site du film, du réalisateur, du distributeur...) et adresses de sites tiers liés au film (critique, forum...)', 'docalist-biblio');
-
-        $fields['relation']['label'] = __('Notices associées', 'docalist-biblio');
-        $fields['relation']['description'] = __("Relations entre ce film et d'autres notices de la base.", 'docalist-biblio');
-
-        // Contruit notre schéma
+class Film extends Reference
+{
+    public static function loadSchema()
+    {
         return [
             'name' => 'film',
             'label' => __('Film', 'docalist-biblio'),
             'description' => __('Un film distribué par un éditeur ou un diffuseur.', 'docalist-biblio'),
-            'fields' => $fields,
+            'fields' => [
+                'title' => [
+                    'label'         => __('Titre du film', 'docalist-biblio'),
+                    'description'   => __('Titre exact du film.', 'docalist-biblio'),
+                ],
+                'genre' => [
+                    'label'         => __('Type de film', 'docalist-biblio'),
+                    'description'   => __(
+                        "Type de film : documentaire, fiction, reportage, clip, congrès, film d'animation...",
+                        'docalist-biblio'
+                    ),
+                ],
+                'media' => [
+                    'label'         => __('Support de diffusion', 'docalist-biblio'),
+                    'description'   => __(
+                        'Support physique utilisé pour la diffusion du film : dvd, vhs, en ligne...',
+                        'docalist-biblio'
+                    ),
+                ],
+                'extent' => [
+                    'label'         => __('Durée du film', 'docalist-biblio'),
+                    'default'       => [['type' => 'minutes']],
+                ],
+                'author' => [
+                    'label'         => __('Équipe technique et casting', 'docalist-biblio'),
+                    'description'   => __(
+                        "Personnes qui ont contribué à l'élaboration du film : réalisateur, scénariste, acteurs...",
+                        'docalist-biblio'
+                    ),
+                ],
+                'organisation' => [
+                    'description'   => __(
+                        'Organismes considéré comme auteur, organisme commanditaire, financeur, producteur...',
+                        'docalist-biblio'
+                    ),
+                ],
+                'othertitle' => [
+                    'description'   => __(
+                        "Autres titres du film : variante, sous-titre...",
+                        'docalist-biblio'
+                    ),
+                ],
+                'translation' => [
+                    'label'         => __("Titre dans d'autres langues", 'docalist-biblio'),
+                    'description'   => __(
+                        'Titre original, traduction du titre dans une autre langue...',
+                        'docalist-biblio'
+                    ),
+                ],
+                'date' => [
+                    'label'         => __('Date du film', 'docalist-biblio'),
+                    'description'   => __(
+                        "Dates au format <code>AAAAMMJJ</code> : sortie, enregistrement, première diffusion...",
+                        'docalist-biblio'
+                    ),
+                ],
+                'language' => [
+                    'description'   => __(
+                        'Langue des pistes audio disponibles.',
+                        'docalist-biblio'
+                    ),
+                ],
+                'format' => [
+                    'label'         => __('Autres caractéristiques', 'docalist-biblio'),
+                    'description'   => __(
+                        'Format de la vidéo, sous-titres disponibles, couleur/N&B, VO/VF, bonus...',
+                        'docalist-biblio'
+                    ),
+                ],
+                'number' => [
+                    'description'   => __(
+                        "Numéro de dépôt légal, visa d'exploitation, numéro de saison, épisode, opus...",
+                        'docalist-biblio'
+                    ),
+                ],
+                'editor' => [
+                    'label'         => __('Editeur ou distributeur', 'docalist-biblio'),
+                    'description'   => __(
+                        "Organismes chargés de la diffusion, la distribution ou la commercialisation du film.",
+                        'docalist-biblio'
+                    ),
+                ],
+                'edition' => [
+                    'label'         => __('Mentions de version', 'docalist-biblio'),
+                    'description'   => __(
+                        "Informations sur la version du film : version longue, version expurgée...",
+                        'docalist-biblio'
+                    ),
+                ],
+                'collection' => [
+                    'label'         => __('Série ou collection', 'docalist-biblio'),
+                    'description'   => __(
+                        "Série ou collection dont fait partie le film et numéro éventuel associé.",
+                        'docalist-biblio'
+                    ),
+                ],
+                'content' => [
+                    'description'   => __(
+                        "Synopsis, résumé, critique, avertissement sur le contenu, objectifs pédagogiques...",
+                        'docalist-biblio'
+                    ),
+                ],
+                'link' => [
+                    'description'   => __(
+                        'Extrait, bande annonce, site du film, site du distributeur, forum...)',
+                        'docalist-biblio'
+                    ),
+                ],
+                'journal'   => ['unused' => true],
+            ],
         ];
     }
 
-    static public function editGrid() {
-        return new Schema([
-            'label' => __('Formulaire de saisie', 'docalist-biblio'),
-            'description' => __("Grille de saisie d'un film.", 'docalist-biblio'),
-            'fields' => [
-                // Title, OtherTitle, Translation
-                'group2' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Titres'],
-                'title',
-                'translation',
-                'othertitle',
-
-                // Author, Organisation
-                'group3' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Personnes et organismes ayant contribué au film'],
-                'author',
-                'organisation',
-                'editor',
-
-                // Type, Genre, Media
-                'group1' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Type de film et support'],
-                'genre',
-                'media',
-
-                // Event
-                'group6' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => __("Film réalisé à l'occasion d'un événement", 'docalist-biblio')],
-                'event',
-
-                // Date / Language / Pagination / Format
-                'group4' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Caractéristiques'],
-                'date',
-                'extent' => [
-                    'default' => [['type' => 'minutes']],
-                ],
-                'language',
-                'edition',
-                'format',
-
-                // Collection et autres numéros
-                'group5' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Collection et numéros'],
-                'collection',
-                'number',
-
-                // Topic / Abstract / Note
-                'group7' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'De quoi parle le film ?'],
-                'content',
-                'topic',
-
-                // // Liens et relations
-                'group8' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Liens et relations'],
-                'link',
-                'relation',
-
-                // Ref / Owner / Creation / Lastupdate
-                'group9' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Informations de gestion'],
-                'type',
-                'ref',
-                'owner',
-            ]
+    public static function getEditGrid()
+    {
+        return static::buildEditGrid([
+            __('Type de film et support', 'docalist-biblio')        => 'genre,media',
+            __('Titres', 'docalist-biblio')                         => 'title,othertitle,translation',
+            __('Distribution et production', 'docalist-biblio')     => 'author,organisation,editor',
+            __("Événement à l'origine du film", 'docalist-biblio')  => 'event',
+            __('Caractéristiques', 'docalist-biblio')               => 'date,extent,language,edition,format',
+            __('Collection et numéros', 'docalist-biblio')          => 'collection,number',
+            __('Ce dont parle le film', 'docalist-biblio')          => 'topic,content',
+            __('Liens et relations', 'docalist-biblio')             => 'link,relation',
+            __('Informations de gestion', 'docalist-biblio')        => '-,type,ref,owner',
         ]);
-
-        /*
-         posttype
-         creation
-         lastupdate
-         password
-         parent
-         slug
-         imported
-         errors
-         */
     }
+
 /*
     static public function contentGrid() {
         return new Schema([
