ff51fb8e9d34ade1d96fd085c3c8e395eb796662
Daniel Ménard <daniel.menard@laposte.net>
2017-06-09 15:14:00 +0200

    BookChapter : ré-écriture schéma avec unused + getEditGrid()

--- class/Reference/BookChapter.php
+++ class/Reference/BookChapter.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -14,7 +14,6 @@
 namespace Docalist\Biblio\Reference;
 
 use Docalist\Biblio\Reference;
-use Docalist\Schema\Schema;
 
 /**
  * Chapitre de livre.
@@ -33,83 +32,35 @@ use Docalist\Schema\Schema;
  * - a un parent de type Book
  * - a une pagination de type "page de début - page de fin"
  */
-class BookChapter extends Reference {
-    static public function loadSchema() {
-        // Récupère les champs d'une référence standard
-        $fields = parent::loadSchema()['fields'];
-
-        // Supprime les champs qu'on n'utilise pas
-        unset($fields['genre']);
-        unset($fields['media']);
-        unset($fields['journal']);
-        unset($fields['editor']);
-        unset($fields['collection']);
-        unset($fields['edition']);
-        unset($fields['event']);
-
-        // Personnalise les tables, les libellés, les description, etc.
-        // todo
-
-        // Contruit notre schéma
+class BookChapter extends Reference
+{
+    public static function loadSchema()
+    {
         return [
             'name' => 'book-chapter',
             'label' => __('Chapitre de livre', 'docalist-biblio'),
             'description' => __('Un chapitre extrait d\'un livre publié.', 'docalist-biblio'),
-            'fields' => $fields,
+            'fields' => [
+                'genre'         => ['unused' => true],
+                'media'         => ['unused' => true],
+                'journal'       => ['unused' => true],
+                'editor'        => ['unused' => true],
+                'collection'    => ['unused' => true],
+                'edition'       => ['unused' => true],
+                'event'         => ['unused' => true],
+            ],
         ];
     }
 
-    static public function editGrid() {
-        return new Schema([
-            'label' => __('Formulaire de saisie', 'docalist-biblio'),
-            'description' => __("Grille de saisie pour un chapitre de livre.", 'docalist-biblio'),
-            'fields' => [
-                // Title, OtherTitle, Translation
-                'group1' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Titres'],
-                'title',
-                'othertitle',
-                'translation',
-
-                // Author, Organisation
-                'group2' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Auteurs'],
-                'author',
-                'organisation',
-
-                // Date / Language / Pagination / Format
-                'group3' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Informations bibliographiques'],
-                'date',
-                'language',
-                'number',
-                'extent',
-                'format',
-
-                // Topic / Abstract / Note
-                'group4' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Indexation et résumé'],
-                'topic',
-                'content',
-
-                // // Liens et relations
-                'group5' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Liens et relations'],
-                'link',
-                'relation',
-
-                // Ref / Owner / Creation / Lastupdate
-                'group6' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Informations de gestion'],
-                'type',
-                'ref',
-                'owner',
-/*
- posttype
- creation
- lastupdate
- password
- parent
- slug
- imported
- errors
- */
-
-            ]
+    public static function getEditGrid()
+    {
+        return static::buildEditGrid([
+            __('Titres', 'docalist-biblio')                         => 'title,othertitle,translation',
+            __('Auteurs', 'docalist-biblio')                        => 'author,organisation',
+            __('Informations bibliographiques', 'docalist-biblio')  => 'date,language,number,extent,format',
+            __('Indexation et résumé', 'docalist-biblio')           => 'topic,content',
+            __('Liens et relations', 'docalist-biblio')             => 'link,relation',
+            __('Informations de gestion', 'docalist-biblio')        => '-,type,ref,owner',
         ]);
     }
 }
