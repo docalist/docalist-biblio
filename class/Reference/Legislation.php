9ed73b98bbbfb5c4c71101c1c76a55154a1af9db
Daniel Ménard <daniel.menard@laposte.net>
2017-06-12 14:58:13 +0200

    Legislation : ré-écriture schéma avec unused + getEditGrid()

--- class/Reference/Legislation.php
+++ class/Reference/Legislation.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -14,7 +14,6 @@
 namespace Docalist\Biblio\Reference;
 
 use Docalist\Biblio\Reference;
-use Docalist\Schema\Schema;
 
 /**
  * Législation.
@@ -30,90 +29,35 @@ use Docalist\Schema\Schema;
  * - a une date de dépôt
  * - est publié ou nom au bo, au jo, etc.
  */
-class Legislation extends Reference {
-    static public function loadSchema() {
-        // Récupère les champs d'une référence standard
-        $fields = parent::loadSchema()['fields'];
-
-        // Supprime les champs qu'on n'utilise pas
-        unset($fields['othertitle']);
-        unset($fields['translation']);
-        unset($fields['editor']);
-        unset($fields['collection']);
-        unset($fields['event']);
-
-        // Personnalise les tables, les libellés, les description, etc.
-        // todo
-
-        // Contruit notre schéma
+class Legislation extends Reference
+{
+    public static function loadSchema()
+    {
         return [
             'name' => 'legislation',
             'label' => __('Législation', 'docalist-biblio'),
             'description' => __('Un texte législatif ou réglementaire.', 'docalist-biblio'),
-            'fields' => $fields,
+            'fields' => [
+                'othertitle'    => ['unused' => true],
+                'translation'   => ['unused' => true],
+                'editor'        => ['unused' => true],
+                'collection'    => ['unused' => true],
+                'event'         => ['unused' => true],
+            ],
         ];
     }
 
-    static public function editGrid() {
-        return new Schema([
-            'label' => __('Formulaire de saisie', 'docalist-biblio'),
-            'description' => __("Grille de saisie pour un texte législatif ou réglementaire.", 'docalist-biblio'),
-            'fields' => [
-                // Type, Genre, Media
-                'group1' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Nature du document'],
-                'genre',
-                'media',
-
-                // Title, OtherTitle, Translation
-                'group2' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Titres'],
-                'title',
-//                 'othertitle',
-//                 'translation',
-
-                // Author, Organisation
-                'group3' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Auteurs'],
-                'author',
-                'organisation',
-
-                // Journal, Number, Date, Edition
-                'group4' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Journal / Périodique'],
-                'journal',
-                'number',
-                'edition',
-
-                // Date / Language / Pagination / Format
-                'group5' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Informations bibliographiques'],
-                'date',
-                'language',
-                'extent',
-                'format',
-
-                // Topic / Abstract / Note
-                'group6' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Indexation et résumé'],
-                'topic',
-                'content',
-
-                // Liens et relations
-                'group7' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Liens et relations'],
-                'link',
-                'relation',
-
-                // Ref / Owner / Creation / Lastupdate
-                'group8' => ['type' => 'Docalist\Biblio\Type\Group', 'label' => 'Informations de gestion'],
-                'type',
-                'ref',
-                'owner',
-            ]
+    public static function getEditGrid()
+    {
+        return static::buildEditGrid([
+            __('Nature du document', 'docalist-biblio')             => 'genre,media',
+            __('Titres', 'docalist-biblio')                         => 'title',
+            __('Auteurs', 'docalist-biblio')                        => 'author,organisation',
+            __('Journal / Périodique', 'docalist-biblio')           => 'journal,number,edition',
+            __('Informations bibliographiques', 'docalist-biblio')  => 'date,language,extent,format',
+            __('Indexation et résumé', 'docalist-biblio')           => 'topic,content',
+            __('Liens et relations', 'docalist-biblio')             => 'link,relation',
+            __('Informations de gestion', 'docalist-biblio')        => '-,type,ref,owner',
         ]);
-/*
- posttype
- creation
- lastupdate
- password
- parent
- slug
- imported
- errors
- */
     }
 }
