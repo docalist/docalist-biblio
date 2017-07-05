90eec9ea5b08837bacad6d003c5b3533f1fe71d2
Daniel Ménard <daniel.menard@laposte.net>
2017-07-05 09:47:15 +0200

    Field/Media : cleanup + schéma par défaut

--- class/Field/Media.php
+++ class/Field/Media.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -16,19 +16,21 @@ namespace Docalist\Biblio\Field;
 use Docalist\Type\TableEntry;
 
 /**
- * Un support de document.
+ * Support du document.
+ *
+ * Ce champ permet d'indiquer le support physique sur lequel réside le document catalogué : pour un livre, par
+ * exemple, il permet d'indiquer s'il s'agit d'un livre broché, d'un DVD, d'une clé USB, etc.
+ *
+ * Le champ est associé à une table d'autorité qui indique les valeurs possibles ("thesaurus:medias" par défaut).
  */
 class Media extends TableEntry
 {
-/*
-    public function setupMapping(MappingBuilder $mapping)
+    public static function loadSchema()
     {
-        $mapping->addField('media')->text()->filter();
+        return [
+            'label' => __('Support', 'docalist-biblio'),
+            'description' => __('Support physique du document (imprimé, numérique, dvd...)', 'docalist-biblio'),
+            'table' => 'thesaurus:medias',
+        ];
     }
-
-    public function mapData(array & $document)
-    {
-        $document['media'][] = $this->getEntryLabel();
-    }
-*/
 }
