b76a00aeea0d34a1971add64703024e65fc41e15
Daniel Ménard <daniel.menard@laposte.net>
2017-07-05 09:49:56 +0200

    Translation : devient un TypedText (BC Break)
    - auparavant on héritait de MultiField
    - on hérite maintenant de TypedText et on hérite directement des
    formats d'affichage disponibles
    - auparavant le multifield avait les champs (language,title), désormais,
    c'est (type,text)
    - surcharge la méthode assign() pour gérer la compatibilité ascendante

--- class/Field/Translation.php
+++ class/Field/Translation.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -13,85 +13,74 @@
  */
 namespace Docalist\Biblio\Field;
 
-use Docalist\Type\MultiField;
-use InvalidArgumentException;
+use Docalist\Biblio\Type\TypedText;
+use Docalist\Type\TableEntry;
+use Docalist\Type\Text;
 
 /**
- * Une traduction du titre original du document.
+ * Traduction du titre original du document.
  *
- * @property Docalist\Type\TableEntry $language
- * @property Docalist\Type\Text $title
+ * Ce champ permet de traduire le titre original du document dans une autre langue. Par exemple si le titre original
+ * du document est en anglais, ça permet d'indiquer la traduction en français ou dans une autre langue.
+ *
+ * Chaque traduction comporte deux sous-champs :
+ * - `type` : langue de la traduction,
+ * - `value` : titre traduit.
+ *
+ * Le sous-champ type est associé à une table d'autorité qui indique les langues possibles (table des langues de
+ * l'union européenne par défaut).
+ *
+ * @property TableEntry $type   Langue de la traduction.
+ * @property Text       $value  Titre traduit.
  */
-class Translation extends MultiField {
-    static public function loadSchema() {
-        // @formatter:off
+class Translation extends TypedText
+{
+    public static function loadSchema()
+    {
         return [
+            'label' => __('Titre traduit', 'docalist-biblio'),
+            'description' => __(
+                'Traduction en une ou plusieurs langues du titre original du document.',
+                'docalist-biblio'
+            ),
             'fields' => [
-                'language' => [
-                    'type' => 'Docalist\Type\TableEntry',
+                'type' => [ // s'appellait "language" avant
                     'label' => __('Langue', 'docalist-biblio'),
                     'table' => 'table:ISO-639-2_alpha3_EU_fr',
                 ],
-                'title' => [
-                    'type' => 'Docalist\Type\Text',
-                    'label' => __('Titre traduit', 'docalist-biblio'),
+                'value' => [ // s'appellait "title" avant
+                    'label' => __('Traduction', 'docalist-biblio'),
                 ]
             ]
         ];
-        // @formatter:on
     }
 
-    protected function getCategoryField()
+    /**
+     * Compatibilité : le type Translation est devenu un TypedText standard.
+     *
+     * Le nom des sous-champs a changé (06/17) : avant, le champ 'type' s'appellait 'language' et le champ 'value'
+     * s'appellait 'title'.
+     *
+     * Pour gérer la compatibilité ascendante (chargement d'une notice contenant les anciens noms de champs), on
+     * surcharge la méthode assign() héritée de Composite et on traduit à la volée les noms des sous-champs.
+     *
+     * Cette méthode pourra être supprimée lorsque les bases Prisme auront été converties.
+     *
+     * {@inheritDoc}
+     */
+    public function assign($value)
     {
-        return 'language';
-    }
-/*
-    public function setupMapping(MappingBuilder $mapping)
-    {
-        $mapping->addField('translation')->text();
-    }
+        ($value instanceof Any) && $value = $value->getPhpValue();
 
-    public function mapData(array & $document) {
-        $document['translation'][] = $this->title();
-    }
-*/
-    public function getAvailableFormats()
-    {
-        return [
-            't' => 'Traduction',
-            'l : t' => 'langue : Traduction',
-            'l: t' => 'langue : Traduction',
-            't (l)' => 'Traduction (langue)',
-        ];
-    }
-
-    public function getFormattedValue($options = null)
-    {
-        $format = $this->getOption('format', $options, $this->getDefaultFormat());
-
-        $language = $this->formatField('language', $options);
-        $title = $this->formatField('title', $options);
-
-        switch ($format) {
-            case 't':       return $title;
-            case 'l : t':   return $language . ' : ' . $title; // espace insécable avant le ':'
-            case 'l: t':    return $language . ': ' . $title;
-            case 't (l)':   return empty($language) ? $title : $title . ' ('  . $language . ')'; // espace insécable avant '('
+        if (is_array($value)) {
+            foreach (['language' => 'type', 'title' => 'value'] as $oldName => $newName) {
+                if (isset($value[$oldName])) {
+                    $value[$newName] = $value[$oldName];
+                    unset($value[$oldName]);
+                }
+            }
         }
 
-        throw new InvalidArgumentException("Invalid Translation format '$format'");
-    }
-
-    public function filterEmpty($strict = true) {
-        // Supprime les éléments vides
-        $empty = parent::filterEmpty();
-
-        // Si tout est vide ou si on est en mode strict, terminé
-        if ($empty || $strict) {
-            return $empty;
-        }
-
-        // Retourne true si on a la langue mais pas le titre traduit
-        return $this->filterEmptyProperty('title');
+        return parent::assign($value);
     }
 }
