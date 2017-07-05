b3412eae851f3db94e7543e49e6965d8cefa8ef4
Daniel Ménard <daniel.menard@laposte.net>
2017-07-05 09:44:24 +0200

    Editor : hérite de organisation
    - avant on avait énormément de code en double entre Editor et
    Organisation
    - sur le fond, un éditeur est un organisme
    - on hérite maintenant de Organisation
    - ce qui supprime tout le code redondant

--- class/Field/Editor.php
+++ class/Field/Editor.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -13,99 +13,42 @@
  */
 namespace Docalist\Biblio\Field;
 
-use Docalist\Type\MultiField;
-
 /**
- * Editeur
+ * Éditeur.
  *
- * @property Docalist\Type\Text $name
- * @property Docalist\Type\Text $city
- * @property Docalist\Type\TableEntry $country
- * @property Docalist\Type\TableEntry $role
+ * Ce champ permet de saisir les organismes en charge de l'édition, de la diffusion et de la distribution
+ * du document
+ *
+ * On peut également indiquer pour chaque organisme une étiquette qui précise son rôle (éditeur, diffuseur...)
+ *
+ * Chaque organisme comporte cinq sous-champs :
+ * - `name` : nom de l'organisme,
+ * - `acronym` : sigle ou acronym éventuel,
+ * - `city` : ville,
+ * - `country` : pays,
+ * - `role` : étiquette de rôle éventuelle.
+ *
+ * Le sous-champ `country` est associé à une table d'autorité qui contient les codes pays disponibles
+ * (par défaut, il s'agit de la table des codes ISO à trois lettres).
+ *
+ * Le sous-champ `role` est associé à une table d'autorité qui contient les étiquettes de rôles disponibles
+ * (par défaut, il s'agit de la table "marc21 relators").
  */
-class Editor extends MultiField {
-    static public function loadSchema() {
-        // @formatter:off
+class Editor extends Organisation
+{
+    public static function loadSchema()
+    {
         return [
-            'category-field' => 'role', // voir si ça marche, enlever getCategoryField() si c'est le cas
+            'label' => __('Editeurs', 'docalist-biblio'),
+            'description' => __(
+                "Organisme délégué par l'auteur pour assurer la diffusion et la distribution du document.",
+                'docalist-biblio'
+            ),
             'fields' => [
                 'name' => [
-                    'type' => 'Docalist\Type\Text',
-                    'label' => __('Nom', 'docalist-biblio'),
                     'description' => __("Nom de l'éditeur", 'docalist-biblio'),
                 ],
-                'city' => [
-                    'type' => 'Docalist\Type\Text',
-                    'label' => __('Ville', 'docalist-biblio'),
-                    'description' => __("Ville de l'éditeur", 'docalist-biblio'),
-                ],
-                'country' => [
-                    'type' => 'Docalist\Type\TableEntry',
-                    'table' => 'table:ISO-3166-1_alpha2_fr',
-                    'label' => __('Pays', 'docalist-biblio'),
-                    'description' => __("Pays d'édition", 'docalist-biblio'),
-                ],
-                'role' => [
-                    'type' => 'Docalist\Type\TableEntry',
-                    'table' => 'thesaurus:marc21-relators_fr',
-                    'label' => __('Rôle', 'docalist-biblio'),
-                    'description' => __('Fonction', 'docalist-biblio'),
-                ]
             ]
         ];
-        // @formatter:on
-    }
-
-    protected function getCategoryField()
-    {
-        return 'role';
-    }
-/*
-    public function setupMapping(MappingBuilder $mapping)
-    {
-        $mapping->addField('editor')->text()->filter()->suggest(); // stemming sur les noms d'organismes
-    }
-
-    public function mapData(array & $document) {
-        $document['editor'][] = $this->name() . '¤' . $this->city() . '¤' . $this->country();
-    }
-*/
-    protected static function initFormats() {
-        self::registerFormat('n, t, c, r', "Nom de l'éditeur, ville, pays, rôle", function(Editor $ed, Editors $parent) {
-            $h = $ed->name();
-
-            if (isset($ed->city)) {
-                $h && $h .= ', ';
-                $h .= $ed->city();
-            }
-
-            if (isset($ed->country)) {
-                $h && $h .= ', ';
-                //$h .= $ed->country();
-                $h .= $parent->lookup($ed->country()); // table1
-            }
-
-            if (isset($ed->role)) {
-                $h && $h .= ' / '; // espaces insécables
-                $h .= $parent->lookup($ed->role(), true); // table2
-            }
-            return $h;
-        });
-        self::registerFormat('name', 'Nom uniquement', function(Editor $ed) {
-            return $ed->name();
-        });
-    }
-
-    public function filterEmpty($strict = true) {
-        // Supprime les éléments vides
-        $empty = parent::filterEmpty();
-
-        // Si tout est vide ou si on est en mode strict, terminé
-        if ($empty || $strict) {
-            return $empty;
-        }
-
-        // Retourne true si on n'a pas de nom
-        return $this->filterEmptyProperty('name');
     }
 }
