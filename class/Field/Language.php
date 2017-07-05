f49fa11c09d39784c2e3517075b905a5febe6240
Daniel Ménard <daniel.menard@laposte.net>
2017-07-05 09:47:02 +0200

    Field/Language : cleanup + schéma par défaut

--- class/Field/Language.php
+++ class/Field/Language.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -16,18 +16,25 @@ namespace Docalist\Biblio\Field;
 use Docalist\Type\TableEntry;
 
 /**
- * Une langue.
+ * Langue du document.
+ *
+ * Ce champ permet de préciser la langue du document catalogué : la langue des textes pour un document écrit, les
+ * pistes audio disponibles pour un film, etc.
+ *
+ * Le champ est associé à une table d'autorité qui indique les langues possibles (par défaut : codes ISO à deux
+ * lettres des langues de l'Union Européenne).
  */
 class Language extends TableEntry
 {
-/*
-    public function setupMapping(MappingBuilder $mapping)
+    public static function loadSchema()
     {
-        $mapping->addField('language')->text()->filter();
+        return [
+            'label' => __('Langue', 'docalist-biblio'),
+            'description' => __(
+                'Langue du document : langue des textes qui figurent dans le document, pistes audio disponibles...',
+                'docalist-biblio'
+            ),
+            'table' => 'table:ISO-639-2_alpha3_EU_fr',
+        ];
     }
-
-    public function mapData(array & $document) {
-        $document['language'][] = $this->getEntryLabel();
-    }
-*/
 }
