ff27c4bbfe3a39c46db6c8feccd802dc83cdfabc
Daniel Ménard <daniel.menard@laposte.net>
2017-07-05 09:41:35 +0200

    Edition : label et description par défaut dans le schéma

--- class/Field/Edition.php
+++ class/Field/Edition.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -16,19 +16,21 @@ namespace Docalist\Biblio\Field;
 use Docalist\Type\Text;
 
 /**
- * Une mention d'édition
+ * Mention d'édition.
+ *
+ * Ce champ permet de préciser s'il s'agit d'une nouvelle édition, d'une édition revue et corrigée, d'une version
+ * expurgée, etc.
  */
 class Edition extends Text
 {
-/*
-    public function setupMapping(MappingBuilder $mapping)
+    public static function loadSchema()
     {
-        $mapping->addField('edition')->text();
+        return [
+            'label' => __("Mentions d'édition", 'docalist-biblio'),
+            'description' => __(
+                "Nouvelle édition, édition revue et corrigée, version expurgée...",
+                'docalist-biblio'
+            ),
+        ];
     }
-
-    public function mapData(array & $document)
-    {
-        $document['edition'][] = $this->value();
-    }
-*/
 }
