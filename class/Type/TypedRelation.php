b8a390a12f48bf40b89c7de306f6a9c4aebff9cb
Daniel Ménard <daniel.menard@laposte.net>
2017-07-05 09:26:02 +0200

    TypedRelation : revoit le schéma (label et description par défaut)

--- class/Type/TypedRelation.php
+++ class/Type/TypedRelation.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -14,25 +14,31 @@
 namespace Docalist\Biblio\Type;
 
 use Docalist\Type\TableEntry;
+use Docalist\Biblio\Type\Relation;
 
 /**
  * Une relation typée : un type composite associant un type provenant d'une table d'autorité à un champ de type
  * Relation.
  *
- * @property TableEntry $type   Type
- * @property Relation   $value  Value
+ * @property TableEntry $type   Type    Type de relation.
+ * @property Relation   $value  Value   Post ID de la fiche liée.
  */
 class TypedRelation extends TypedText
 {
-    static public function loadSchema() {
+    public static function loadSchema()
+    {
         return [
-            'label' => __('Relation', 'docalist-core'),
-            'description' => __('Relation vers une autre fiche et type de relation.', 'docalist-core'),
-            'editor' => 'table',
+            'label' => __('Relation', 'docalist-biblio'),
+            'description' => __('Relation vers une autre fiche et type de relation.', 'docalist-biblio'),
             'fields' => [
+                'type' => [
+                    'table' => 'table:relations',
+                    'description' => __('Type de relation', 'docalist-biblio'),
+                ],
                 'value' => [
                     'type' => 'Docalist\Biblio\Type\Relation',
-                    'label' => __('Fiche', 'docalist-core'),
+                    'label' => __('Fiche liée', 'docalist-biblio'),
+                    'description' => __('Post ID de la fiche liée', 'docalist-biblio'),
                 ],
             ],
         ];
