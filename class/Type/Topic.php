b99e96fea925b504c31abf012c60bf4cc54254ed
Daniel Ménard <daniel.menard@laposte.net>
2017-07-05 09:31:54 +0200

    Topic : devient un TypedText standard(BC Break)
    - auparavant la classe Topic était un MultiField (type,term*)
    - désormais, c'est un TypedText (type,text*)
    - ajout de plusieurs méthodes (assign, __get, __set, __call...) pour
    gérer la compatibilité ascendante
    - clarifie l'endroit du schéma où la table des topics doit être déclarée
    (dans le schéma du champ type désormais).

--- class/Type/Topic.php
+++ class/Type/Topic.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -13,63 +13,97 @@
  */
 namespace Docalist\Biblio\Type;
 
-use Docalist\Type\MultiField;
 use Docalist\Table\TableManager;
 use Docalist\Table\TableInterface;
 use Docalist\Forms\TopicsInput;
 use InvalidArgumentException;
-use Docalist\Type\TableEntry;
 use Docalist\Type\Text;
 
 /**
  * Une liste de mots-clés d'un certain type.
  *
- * @property TableEntry $type
- * @property Text[] $term
+ * @property TableEntry $type   Type    Vocabulaire.
+ * @property Text[]     $value  Value   Tags.
+ *
  */
-class Topic extends MultiField
+class Topic extends TypedText
 {
-    /**
-     * Collection parent (pour avoir accès à la table des topics)
-     *
-     * @var Topics
-     */
-    protected $parent;
-
     public static function loadSchema()
     {
         return [
+
             'label' => __('Indexation', 'docalist-biblio'),
-            'description' => __('Tags, mots-clés, catégories, domaines, étiquettes, classification...', 'docalist-biblio'),
-            'table' => 'table:topics', // non, dans la collection
-            'editor' => 'table',
-            'key' => 'type',
+            'description' => __(
+                'Mots-clés, tags et étiquettes permettant de classer le document et de décrire son contenu.',
+                'docalist-biblio'
+            ),
             'fields' => [
                 'type' => [
-                    'type' => 'Docalist\Type\TableEntry',
-                    'table' => 'table:topics',
                     'label' => __('Vocabulaire', 'docalist-biblio'),
+                    'table' => 'table:topics',
                 ],
-                'term' => [
-                    'type' => 'Docalist\Type\Text*',
+                'value' => [
+                    'type' => 'Docalist\Type\Text*', // Monovalué dans TypedText, on le rend répétable
                     'label' => __('Termes', 'docalist-biblio'),
                 ],
             ],
+            'key' => 'type', // La collection indexe les éléments par type de topic
         ];
     }
 
-    /**
-     * @return Topics|null
-     */
-    public function getParent()
+    /* ------------------------------------------------------------------------------------------------------------
+     * COMPATIBILITE ASCENDANTE 21/06/17 :
+     *
+     * - le type Topic est devenu un TypedText standard
+     * - le sous-champ 'term' s'appelle maintenant 'value'
+     *
+     * Les méthodes qui suivent sont là uniquement pour assurer la compatibilité ascendante. Elles pourront être
+     * supprimées une fois que prisme et svb auront été adaptés.
+     * ------------------------------------------------------------------------------------------------------------ */
+    public function assign($value)
     {
-        return $this->parent;
+        ($value instanceof Any) && $value = $value->getPhpValue();
+
+        if (is_array($value)) {
+            foreach (['term' => 'value'] as $oldName => $newName) {
+                if (isset($value[$oldName])) {
+                    $value[$newName] = $value[$oldName];
+                    unset($value[$oldName]);
+                }
+            }
+        }
+
+        return parent::assign($value);
     }
 
-    public function setParent(Topics $parent)
+    public function __set($name, $value)
     {
-        $this->parent = $parent;
+        return parent::__set($name === 'term' ? 'value' : $name, $value);
     }
+
+    public function __isset($name)
+    {
+        return parent::__isset($name === 'term' ? 'value' : $name);
+    }
+
+    public function __unset($name)
+    {
+        return parent::__unset($name === 'term' ? 'value' : $name);
+    }
+
+    public function __get($name)
+    {
+        return parent::__get($name === 'term' ? 'value' : $name);
+    }
+
+    public function __call($name, $arguments)
+    {
+        return parent::__call($name === 'term' ? 'value' : $name, $arguments);
+    }
+
+    /* ------------------------------------------------------------------------------------------------------------
+     * FIN COMPATIBILITE ASCENDANTE
+     * ------------------------------------------------------------------------------------------------------------ */
 
     public function getAvailableEditors()
     {
@@ -81,7 +115,6 @@ class Topic extends MultiField
         $editor = new TopicsInput($this->schema->name(), $this->schema->table());
 
         $editor
-            ->setName($this->schema->name())
             ->setLabel($this->getOption('label', $options))
             ->setDescription($this->getOption('description', $options));
 
@@ -102,18 +135,14 @@ class Topic extends MultiField
     public function getTermsLabel()
     {
         // Récupère la liste des termes
-        $terms = $this->term();
+        $terms = $this->value->getPhpValue();
         $terms = array_combine($terms, $terms);
-
-        // Récupère la table des topics utilisée (dans le schéma de la collection Topics parent)
-        if (is_null($this->parent) || is_null($schema = $this->parent->schema()) || is_null($table = $schema->table())) {
-            return $this->term();
-        }
 
         // Récupère le table-manager
         $tables = docalist('table-manager'); /** @var TableManager $tables */
 
-        // Récupère la table qui contient la liste des vocabulaires
+        // Récupère la table qui contient la liste des vocabulaires (dans le schéma du champ type)
+        $table = $this->schema->getField('type')->table();
         $tableName = explode(':', $table)[1];
         $table = $tables->get($tableName); /** @var TableInterface $table */
 
@@ -143,17 +172,22 @@ class Topic extends MultiField
         switch ($format) {
             case 'v':
                 return implode(', ', $this->getTermsLabel());
+
             case 'V':
-                return implode(', ', $this->term());
+                return implode(', ', $this->value->getPhpValue());
+
             case 't : v':
                 $format = '%s : %s'; // espace insécable avant le ':'
                 break;
+
             case 't: v':
                 $format = '%s: %s';
                 break;
+
             case 'v (t)':
                 $format = '%2$s (%1$s)';
                 break;
+
             default:
                 throw new InvalidArgumentException("Invalid Topic format '$format'");
         }
@@ -163,71 +197,5 @@ class Topic extends MultiField
             $this->type->getEntryLabel(),
             implode(', ', $this->getTermsLabel())
         );
-    }
-
-//     protected static function initFormats() {
-//         self::registerFormat('v', 'Mots-clés', function(Topic $topic, Topics $parent) {
-//             // Récupère la liste des termes
-//             $terms = $topic->term();
-
-//             $tables = docalist('table-manager'); /** @var TableManager $tables */
-
-//             // Récupère la table qui contient la liste des vocabulaires
-//             $tableName = explode(':', $parent->schema->table())[1];
-//             $table = $tables->get($tableName); /** @var TableInterface $table */
-
-//             // Détermine la source qui correspond au type du topic
-//             $source = $table->find('source', 'code='. $table->quote($topic->type()));
-//             if ($source !== false) { // type qu'on n'a pas dans la table topics
-//                 list($type, $tableName) = explode(':', $source);
-
-//                 // Si la source est une table, on traduit les termes
-//                 if ($type === 'table' || $type === 'thesaurus') {
-//                     $table = $tables->get($tableName); /** @var TableInterface $table */
-//                     foreach ($terms as & $term) {
-//                         $result = $table->find('label', 'code=' . $table->quote($term));
-//                         $result !== false && $term = $result;
-//                     }
-//                 }
-//             }
-
-//             // Sinon, on les retourne tels quels
-
-//             return implode(', ', $terms);
-//         });
-
-//         self::registerFormat('V', 'Code des mots-clés (i.e. mots-clés en majuscules)', function(Topic $topic, Topics $parent) {
-//             return implode(', ', $topic->term());
-//         });
-
-//         self::registerFormat('t : v', 'Nom du vocabulaire : Mots-clés', function(Topic $topic, Topics $parent) {
-//             $terms = self::callFormat('v', $topic, $parent);
-//             return $parent->lookup($topic->type()) . ' : ' . $terms;
-//             // espace insécable avant le ':'
-//         });
-
-//         self::registerFormat('t: v', 'Nom du vocabulaire: Mots-clés', function(Topic $topic, Topics $parent) {
-//             $terms = self::callFormat('v', $topic, $parent);
-//             return $parent->lookup($topic->type()) . ': ' . $terms;
-//         });
-
-//         self::registerFormat('v (t)', 'Mots-clés (Nom du vocabulaire)', function(Topic $topic, Topics $parent) {
-//             $terms = self::callFormat('v', $topic, $parent);
-//             return $terms . ' (' . $parent->lookup($topic->type()) . ')';
-//             // espace insécable avant '('
-//         });
-//     }
-
-    public function filterEmpty($strict = true) {
-        // Supprime les éléments vides
-        $empty = parent::filterEmpty();
-
-        // Si tout est vide ou si on est en mode strict, terminé
-        if ($empty || $strict) {
-            return $empty;
-        }
-
-        // Retourne true si on n'a pas de mots-clés
-        return $this->filterEmptyProperty('term');
     }
 }
