936bf6eb408cb35c9a6bb0a5256ae0ee0ba3e3ed
Daniel Ménard <daniel.menard@laposte.net>
2017-07-13 09:57:37 +0200

    PostTitle : ajoute un schéma avec libellé et description par défaut

--- class/Type/PostTitle.php
+++ class/Type/PostTitle.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -16,8 +16,15 @@ namespace Docalist\Biblio\Type;
 use Docalist\Type\Text;
 
 /**
- * Le titre de la fiche.
+ * Le titre WordPress de la fiche.
  */
 class PostTitle extends Text
 {
+    public static function loadSchema()
+    {
+        return [
+            'label' => __('Titre du post', 'docalist-biblio'),
+            'description' => __('Titre du post WordPress.', 'docalist-biblio'),
+        ];
+    }
 }
