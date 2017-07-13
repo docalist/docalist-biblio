911c252bd107b354a0835b19725d0ec5363c5d28
Daniel Ménard <daniel.menard@laposte.net>
2017-07-13 09:53:49 +0200

    PostAuthor : ajoute un schéma avec libellé et description par défaut

--- class/Type/PostAuthor.php
+++ class/Type/PostAuthor.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -21,6 +21,14 @@ use WP_User;
  */
 class PostAuthor extends Text
 {
+    public static function loadSchema()
+    {
+        return [
+            'label' => __('Créé par', 'docalist-biblio'),
+            'description' => __("Nom de login de l'utilisateur WordPress qui a créé la fiche.", 'docalist-biblio'),
+        ];
+    }
+
     public function getFormattedValue($options = null)
     {
         $author = get_user_by('id', $this->getPhpValue()); /** @var WP_User $author */
