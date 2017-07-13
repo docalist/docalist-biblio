f286aadc93d06361d825e13556ebbee849822f71
Daniel Ménard <daniel.menard@laposte.net>
2017-07-13 09:54:32 +0200

    PostStatus : ajoute un schéma avec libellé et description par défaut

--- class/Type/PostStatus.php
+++ class/Type/PostStatus.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -20,4 +20,11 @@ use Docalist\Type\Text;
  */
 class PostStatus extends Text
 {
+    public static function loadSchema()
+    {
+        return [
+            'label' => __('Statut WordPress', 'docalist-biblio'),
+            'description' => __('Statut de la fiche.', 'docalist-biblio'),
+        ];
+    }
 }
