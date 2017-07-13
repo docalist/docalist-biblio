cd55caa5313b0a0bc75ac6ca462e196a79e1e6a2
Daniel Ménard <daniel.menard@laposte.net>
2017-07-13 09:55:04 +0200

    RefNumber : ajoute un schéma avec libellé et description par défaut

--- class/Type/RefNumber.php
+++ class/Type/RefNumber.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -20,4 +20,14 @@ use Docalist\Type\Integer;
  */
 class RefNumber extends Integer
 {
+    public static function loadSchema()
+    {
+        return [
+            'label' => __('Numéro de fiche', 'docalist-biblio'),
+            'description' => __(
+                'Numéro unique attribué par docalist pour identifier la fiche au sein de la collection.',
+                'docalist-biblio'
+            ),
+        ];
+    }
 }
