277557a6a0936ded3d9e418f0b9d8bdcb54226fd
Daniel Ménard <daniel.menard@laposte.net>
2017-07-17 11:23:36 +0200

    Biblio/Field/Title : ajout schéma avec libellé et description par défaut
    +cleanup

--- class/Field/Title.php
+++ class/Field/Title.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -14,24 +14,25 @@
 namespace Docalist\Biblio\Field;
 
 use Docalist\Type\Text;
-use Docalist\Biblio\DatabaseIndexer;
 
 /**
  * Le titre de la notice.
  */
-class Title extends Text {
+class Title extends Text
+{
+    public static function loadSchema()
+    {
+        return [
+            'label' => __('Titre', 'docalist-biblio'),
+            'description' => __(
+                'Titre original du document.',
+                'docalist-biblio'
+            ),
+        ];
+    }
+
     public function getEditorForm($options = null)
     {
         return parent::getEditorForm($options)->addClass('large-text');
     }
-/*
-    public function setupMapping(MappingBuilder $mapping)
-    {
-        DatabaseIndexer::standardMapping('post_title', $mapping);
-    }
-
-    public function mapData(array & $document) {
-        DatabaseIndexer::standardMap('post_title', $this->value(), $document);
-    }
-*/
 }
