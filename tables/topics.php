9d80d21fba213bf5596e65b06ec26c8df1eb8c63
Daniel Ménard <daniel.menard@laposte.net>
2017-10-19 10:30:41 +0200

    Table topics : corrige le nom de l'index pour "free"

--- tables/topics.php
+++ tables/topics.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Biblio" plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -32,5 +32,5 @@
 return [
     ['code', 'label', 'description', 'source'],
 
-    ['free', __('Indexation libre', 'docalist-biblio'), __('Candidats descripteurs', 'docalist-biblio'), 'index:topic.free'],
+    ['free', __('Indexation libre', 'docalist-biblio'), __('Candidats descripteurs', 'docalist-biblio'), 'index:topic-free'],
 ];
