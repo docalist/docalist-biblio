137dff62f55da93a9ca8324ef720586e66a2f728
Daniel Ménard <daniel.menard@laposte.net>
2017-11-11 09:51:36 +0100

    Settings : code style

--- class/Export/Settings.php
+++ class/Export/Settings.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Biblio Export" plugin.
  *
- * Copyright (C) 2015-2015 Daniel Ménard
+ * Copyright (C) 2015-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -25,20 +25,26 @@ class Settings extends TypeSettings
 {
     protected $id = 'docalist-biblio-export';
 
-    static public function loadSchema()
+    public static function loadSchema()
     {
         return [
             'fields' => [
                 'exportpage' => [
                     'type' => 'Docalist\Type\Integer',
                     'label' => __("Page pour l'export", 'docalist-biblio-export'),
-                    'description' => __("Page WordPress sur laquelle l'export sera disponible.", 'docalist-biblio-export'),
+                    'description' => __(
+                        "Page WordPress sur laquelle l'export sera disponible.",
+                        'docalist-biblio-export'
+                    ),
                     'default' => 0,
                 ],
                 'limit' => [
                     'type' => 'Docalist\Biblio\Export\LimitSetting*',
                     'label' => __("Limites de l'export", 'docalist-biblio-export'),
-                    'description' => __('Liste des rôles autorisés à exporter des notices et nombre maximum de notices exportables par rôle.', 'docalist-biblio-export'),
+                    'description' => __(
+                        'Liste des rôles autorisés à exporter des notices et nombre maximum de notices par rôle.',
+                        'docalist-biblio-export'
+                    ),
                     'key' => 'role',
                 ],
             ],
