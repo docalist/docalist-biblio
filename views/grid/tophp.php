4e0b5b285b13bf73d0a02afdfad8beee2080d586
Daniel Ménard <daniel.menard@laposte.net>
2017-11-24 11:33:20 +0100

    gridtophp : code à revoir complètement (remplace par un var_export)

--- views/grid/tophp.php
+++ views/grid/tophp.php
@@ -31,6 +31,9 @@ use Docalist\Schema\Schema;
  * @var Schema              $base       La grille à éditer.
  * @var bool                $diffonly   Mode de comparaison (grille complète / différences uniquement).
  */
+
+echo '<pre>', htmlspecialchars(var_export($grid->getPhpValue(), true)), '</pre>';
+return;
 $urlfull = esc_url($this->url('GridToPhp', $dbindex, $typeindex, $gridname));
 $urldiff = esc_url($this->url('GridToPhp', $dbindex, $typeindex, $gridname, true));
 echo '<pre>', htmlspecialchars(json_encode($grid, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)), '</pre>';
