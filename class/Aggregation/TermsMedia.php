a274b1c82afda75d408b89095116ac38bc592f95
Daniel Ménard <daniel.menard@laposte.net>
2017-11-28 12:42:24 +0100

    Nouvelle agrégation standard TermsMedia

---
+++ class/Aggregation/TermsMedia.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the 'Docalist Biblio' plugin.
+ *
+ * Copyright (C) 2012-2017 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Biblio
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Biblio\Aggregation;
+
+use Docalist\Search\Aggregation\Bucket\TableEntriesAggregation;
+
+/**
+ * Une agrégation standard de type "terms" sur le champ "media".
+ */
+class TermsMedia extends TableEntriesAggregation
+{
+    /**
+     * Constructeur
+     *
+     * @param array $parameters     Autres paramètres de l'agrégation.
+     * @param array $options        Options d'affichage.
+     */
+    public function __construct(array $parameters = [], array $options = [])
+    {
+        !isset($parameters['size']) && $parameters['size'] = 1000;
+        !isset($options['title']) && $options['title'] = __('Media', 'docalist-search');
+        parent::__construct('media.filter', 'medias', $parameters, $options);
+    }
+}
