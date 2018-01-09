3c124342a62004a093dd27695dd163c7c5b0d3d3
Daniel Ménard <daniel.menard@laposte.net>
2018-01-09 11:57:11 +0100

    Plugin : cleanup
    On se contente de déclarer les types de notices définies par le plugin.

--- class/Plugin.php
+++ class/Plugin.php
@@ -13,66 +13,16 @@
  */
 namespace Docalist\Biblio;
 
-use Docalist\Views;
-use Docalist\Biblio\Database;
-use Docalist\Biblio\Reference;
-use Docalist\Biblio\Settings\Settings;
-use Docalist\Biblio\Settings\DatabaseSettings;
-use Docalist\Biblio\Pages\AdminDatabases;
-use Exception;
-use Docalist\Biblio\Export\ExportService;
-
 /**
  * Plugin de gestion de notices bibliographiques.
  */
 class Plugin {
-
-    /**
-     * La configuration du plugin.
-     *
-     * @var Settings
-     */
-    protected $settings;
-
-    /**
-     * La liste des bases
-     *
-     * @var Database[]
-     */
-    protected $databases;
-
     /**
      * Initialise le plugin.
      */
     public function __construct() {
         // Charge les fichiers de traduction du plugin
         load_plugin_textdomain('docalist-biblio', false, 'docalist-biblio/languages');
-
-        // Ajoute notre répertoire "views" au service "docalist-views"
-        add_filter('docalist_service_views', function(Views $views) {
-            return $views->addDirectory('docalist-biblio', DOCALIST_BIBLIO_DIR . '/views');
-        });
-
-        // Déclare le service "docalist-biblio-export"
-        docalist('services')->add('docalist-biblio-export', new ExportService());
-
-        add_action('init', function() {
-            // Charge la configuration du plugin
-            $this->settings = new Settings(docalist('settings-repository'));
-
-            // Crée les bases de données définies par l'utilisateur
-            $this->databases = array();
-            foreach ($this->settings->databases as $settings) {
-                /** @var DatabaseSettings $settings */
-                $database = new Database($settings);
-                $this->databases[$database->postType()] = $database;
-            }
-        });
-
-        // Crée la page Réglages » Docalist-Biblio
-        add_action('admin_menu', function () {
-            new AdminDatabases($this->settings);
-        });
 
         // Déclare la liste des types définis dans ce plugin
         add_filter('docalist_biblio_get_types', function(array $types) {
@@ -92,96 +42,5 @@ class Plugin {
 
             return $types;
         });
-
-        // Nos filtres
-        add_filter('docalist_biblio_get_reference', array($this, 'getReference'));
-
-        // Liste des exporteurs définis dans ce plugin
-        add_filter('docalist_biblio_get_export_formats', function(array $formats) {
-            $formats['docalist-json'] = [
-                'label' => 'Docalist JSON',
-                'description' => 'Fichier JSON compact, notices en format natif de Docalist.',
-                'converter' => 'Docalist\Biblio\Export\Docalist',
-                'exporter' => 'Docalist\Biblio\Export\Json',
-            ];
-
-            $formats['docalist-json-pretty'] = [
-                'label' => 'Docalist JSON formatté',
-                'description' => 'Fichier JSON formatté et indenté, notices en format natif de Docalist.',
-                'converter' => 'Docalist\Biblio\Export\Docalist',
-                'exporter' => 'Docalist\Biblio\Export\Json',
-                'exporter-settings' => [
-                    'pretty' => true,
-                ],
-            ];
-
-            $formats['docalist-xml'] = [
-                'label' => 'Docalist XML',
-                'description' => 'Fichier XML compact, notices en format natif de Docalist.',
-                'converter' => 'Docalist\Biblio\Export\Docalist',
-                'exporter' => 'Docalist\Biblio\Export\Xml',
-            ];
-
-            $formats['docalist-xml-pretty'] = [
-                'label' => 'Docalist XML formatté',
-                'description' => 'Fichier XML formatté et indenté, notices en format natif de Docalist.',
-                'converter' => 'Docalist\Biblio\Export\Docalist',
-                'exporter' => 'Docalist\Biblio\Export\Xml',
-                'exporter-settings' => [
-                    'indent' => 4,
-                ],
-            ];
-
-            return $formats;
-        }, 10);
-
-        // Déclare nos assets
-        require_once dirname(__DIR__) . '/assets/register.php';
-    }
-
-    /**
-     * Retourne la liste des bases de données définies.
-     *
-     * @return Database[]
-     */
-    public function databases() {
-        return $this->databases;
-    }
-
-    /**
-     * Retourne la base de données ayant le post type indiqué.
-     *
-     * @param string $postType Le post type de la base recherchée.
-     *
-     * @return Database|null Retourne l'objet Database ou null si la base
-     * indiquée n'existe pas.
-     */
-    public function database($postType) {
-        return isset($this->databases[$postType]) ? $this->databases[$postType] : null;
-    }
-
-    /**
-     * Retourne l'objet référence dont l'id est passé en paramètre.
-     *
-     * Implémentation du filtre 'docalist_biblio_get_reference'.
-     *
-     * @param string $id POST_ID de la référence à charger.
-     *
-     * @return Reference Retourne un objet Reference si une grille a été
-     * indiquée ; un tableau contenant les données de la notice sinon.
-     *
-     * @throws Exception
-     */
-    public function getReference($id = null) {
-        is_null($id) && $id = get_the_ID();
-        $type = get_post_type($id);
-
-        if (! isset($this->databases[$type])) {
-            $msg = __("Le post %s n'est pas une référence docalist (postype=%s)");
-            throw new Exception(sprintf($msg, $id, $type));
-        }
-
-        $database = $this->databases[$type]; /** @var Database $database */
-        return $database->load($id);
     }
 }
