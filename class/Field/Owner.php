38fb082dbf3b460b41f2d10d1c47e4a84573aed2
Daniel Ménard <daniel.menard@laposte.net>
2018-02-08 10:18:54 +0100

    Field/Owner : name + repeatable + remarques

--- class/Field/Owner.php
+++ class/Field/Owner.php
@@ -12,15 +12,30 @@ namespace Docalist\Biblio\Field;
 use Docalist\Type\Text;
 
 /**
- * Un producteur.
+ * Champ "owner" : titulaire de la notice.
+ *
+ * Champ répétable.
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
 class Owner extends Text
 {
+    /*
+     * Il y a un problème de définition du rôle exact de ce champ. Qu'est-ce qu'il contient exactment ?
+     * - code du producteur de la notice ?
+     * - code de celui qui l'a corrigée ou validée ?
+     * - code du détenteur du document catalogué (i.e. qui a des exemplaires) ?
+     * - code identifiant les comptes qui peuvent éditer la notice ? (en plus de l'auteur wordpress)
+     * etc.
+     *
+     * A revoir.
+     */
+
     public static function loadSchema()
     {
         return [
+            'name' => 'owner',
+            'repeatable' => true,
             'label' => __('Producteur de la notice', 'docalist-biblio'),
             'description' => __(
                 'Personne ou organisme producteur de la notice.',
