96b3fae50ea9658b59e343a0181a1587f6df1cae
Daniel Ménard <daniel.menard@laposte.net>
2018-02-08 09:37:23 +0100

    Field/Media : name + repeatable + tweak phpdoc

--- class/Field/Media.php
+++ class/Field/Media.php
@@ -12,10 +12,10 @@ namespace Docalist\Biblio\Field;
 use Docalist\Type\TableEntry;
 
 /**
- * Support du document.
+ * Champ "media" : mots-clés décrivant le support physique du document.
  *
- * Ce champ permet d'indiquer le support physique sur lequel réside le document catalogué : pour un livre, par
- * exemple, il permet d'indiquer s'il s'agit d'un livre broché, d'un DVD, d'une clé USB, etc.
+ * Ce champ répétable permet de décrire le support physique sur lequel réside le document catalogué : pour un
+ * livre, par exemple, il permet d'indiquer s'il s'agit d'un livre broché, d'un DVD, d'une clé USB, etc.
  *
  * Le champ est associé à une table d'autorité qui indique les valeurs possibles ("thesaurus:medias" par défaut).
  *
@@ -23,9 +23,18 @@ use Docalist\Type\TableEntry;
  */
 class Media extends TableEntry
 {
+    /*
+     * Remarque : sur le fond, ce champ est juste un type particulier d'indexation (d'ailleurs la table par
+     * défaut est un théaurus). On pourrait envisager de le supprimer et d'utiliser le champ "topic" à la place.
+     * On garde un champ distinct pour le moment, à reconsidérer si un jour le champ topic supporte "explode" et
+     * qu'on peut mettre l'indexation "media" où on veut dans le formulaire de saisie.
+     */
+
     public static function loadSchema()
     {
         return [
+            'name' => 'media',
+            'repeatable' => true,
             'label' => __('Support', 'docalist-biblio'),
             'description' => __('Support physique du document (imprimé, numérique, dvd...)', 'docalist-biblio'),
             'table' => 'thesaurus:medias',
