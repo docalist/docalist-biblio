a08db1839b7d58454c56190f38f2e98821987f0c
Daniel Ménard <daniel.menard@laposte.net>
2018-02-09 09:45:49 +0100

    Nouvelle classe LinkField

---
+++ class/Field/LinkField.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the 'Docalist Biblio' plugin.
+ *
+ * Copyright (C) 2012-2017 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+namespace Docalist\Biblio\Field;
+
+use Docalist\Data\Field\LinkField as BaseLinkField;
+
+/**
+ * Champ standard "link" : un lien internet (url, uri, e-mail, hashtag...)
+ *
+ * Ce champ permet de saisir les liens d'une entité..
+ *
+ * Chaque lien comporte quatre sous-champs :
+ * - `type` : type de lien
+ * - `url` : uri,
+ * - `label` : libellé à afficher pour ce lien.
+ * - `date` : date à laquelle le lien a été consulté/vérifié.
+ *
+ * Le sous-champ type est associé à une table d'autorité qui contient les types de liens disponibles
+ * ('table:links'par défaut).
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class LinkField extends BaseLinkField
+{
+    public static function loadSchema()
+    {
+        return [
+            'fields' => [
+                'type' => [
+                    'table' => 'table:links',
+                ],
+            ]
+        ];
+    }
+
+}
